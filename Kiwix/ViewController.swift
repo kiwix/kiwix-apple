29e5eda691de1fd1935b5c6d977b1ff1b337df64
Automactic <christopherliqd@gmail.com>
2015-07-31 17:13:07 -0400

    Initial Commit

---
+++ Kiwix/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Kiwix
+//
+//  Created by Chris Li on 7/31/15.
+//  Copyright © 2015 Chris Li. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
