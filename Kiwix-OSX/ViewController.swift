cbf71a344d1a616eaa00f4c15217ac2b157f3e50
automactic <chrisencoded@gmail.com>
2016-05-17 08:27:48 -0400

    Refactor and add OS X base project

---
+++ Kiwix-OSX/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Kiwix-OSX
+//
+//  Created by Chris Li on 5/17/16.
+//  Copyright © 2016 Chris. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
