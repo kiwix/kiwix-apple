5455018444e24c3b59311bebdccc40fc10430ee2
Chris Li <chris@kiwix.org>
2016-08-12 14:53:42 -0400

    add support for ft-index in zim

--- Kiwix/libkiwix/build/build-zim.sh
+++ Kiwix/libkiwix/build/build-zim.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# regarding export MACOSX_DEPLOYMENT_TARGET="10.4" in build_iOS, see reason why do it like this
+# http://stackoverflow.com/questions/32622284/building-c-static-libraries-using-configure-make-with-fembed-bitcode-fails
+
 ROOT=$(pwd)
 
 ##########################  ZIM  ##########################
@@ -7,11 +10,7 @@ ROOT=$(pwd)
 # git clone https://gerrit.wikimedia.org/r/p/openzim.git
 # modify ffstream.cpp, replace stat64 with stat
 
-LZMAHEADERPATH="/Volumes/Data/Developer/build/xz-5.2.2/build/Universal/include"
-ZIMREADERPATH=$ROOT/openzim/zimlib
-cd $ZIMREADERPATH
-
-# ./autogen.sh
+LZMAHEADERPATH="/Volumes/Data/Developer/Kiwix/Kiwix/libkiwix/include"
 
 build_iOS()
 {
@@ -24,11 +23,16 @@ build_iOS()
 		SDKROOT="$(xcodebuild -version -sdk iphoneos | grep -E '^Path' | sed 's/Path: //')"
 	fi
 
-	export CC="$(xcrun -sdk iphoneos -find clang)"
+	export MACOSX_DEPLOYMENT_TARGET="10.4"
+
+	export CC="$(xcrun -find clang)"
 	export CFLAGS="-fembed-bitcode -isysroot $SDKROOT -arch ${ARCH} -miphoneos-version-min=9.0 -I$LZMAHEADERPATH"
 
 	export CPP="$CC -E"
 	export CPPFLAGS="$CFLAGS"
+
+	export CXX="$(xcrun -find clang++)"
+	export CXXFLAGS="$CFLAGS -stdlib=libc++ -std=c++11"
 
 	export LDFLAGS="-arch ${ARCH} -isysroot $SDKROOT"
 
@@ -63,6 +67,28 @@ build_OSX()
 	make && make install && make clean
 }
 
+distribute_iOS() {
+	iOSBUILDDir=$(pwd)/build/iOS
+	cd $iOSBUILDDir
+	mkdir -p universal/lib
+
+	cd armv7/lib
+	for file in *.a
+	do
+		cd $iOSBUILDDir
+		lipo -create armv7/lib/$file armv7s/lib/$file arm64/lib/$file x86_64/lib/$file i386/lib/$file -output universal/lib/$file
+	done
+
+	# cd armv7/lib
+	# for file in *.dylib
+	# do
+	# 	cd $iOSBUILDDir
+	# 	lipo -create armv7/lib/$file armv7s/lib/$file arm64/lib/$file x86_64/lib/$file i386/lib/$file -output universal/lib/$file
+	# done
+
+	cp -r armv7/include universal
+}
+
 distribute() {
 	cd $ZIMREADERPATH/build
 	mkdir -p Universal/iOS/lib
@@ -92,9 +118,10 @@ distribute() {
 # build_iOS armv7
 # build_iOS armv7s
 # build_iOS arm64
+distribute_iOS
 
 # build_OSX i386
 # build_OSX x86_64
 
-distribute
+# distribute
 
