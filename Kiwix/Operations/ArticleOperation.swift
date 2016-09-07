92ced506ba7dbab8c07f5cbce4ffe9e4aa822f0c
Chris Li <chris@kiwix.org>
2016-09-07 13:19:46 -0400

    ArticleOperation

---
+++ Kiwix/Operations/ArticleOperation.swift
@@ -0,0 +1,13 @@
+//
+//  ArticleOperation.swift
+//  Kiwix
+//
+//  Created by Chris Li on 9/7/16.
+//  Copyright © 2016 Chris. All rights reserved.
+//
+
+import UIKit
+
+class ArticleOperation: NSObject {
+
+}
