5192b61a686be82d3d48a0b60496522c91877eb8
Chris Li <shuowei.li@kmkconsultinginc.com>
2016-10-17 15:36:11 -0400

    toc js

--- Kiwix/JavaScripts/getTableOfContents.js
+++ Kiwix/JavaScripts/getTableOfContents.js
@@ -1,20 +1,24 @@
-function getTOC() {
-	var content = document.getElementById("mw-content-text");
-	var headers = [];
-
-	if (content) {
-		for (var i = 0; i < content.children.length; i++) {
-			var element = content.children[i];
-			if (element.tagName.toUpperCase() == "h2".toUpperCase() || element.tagName.toUpperCase() == "h3".toUpperCase()) {
-				var header = {};
-				header.id = element.getAttribute("id");
-				header.textContent = element.textContent;
-				header.tagName = element.tagName;
-				headers.push(header);
-			}
-		}
-	}
-
-	return headers;
+function getTableOfContents() {
+    var h1 = document.getElementsByTagName('h1')[0];
+    var headers = [elementToObject(h1)];
+    
+    var content = document.getElementById("mw-content-text");
+    var headerTags = ['h2', 'h3', 'h4'].map(function(x){ return x.toUpperCase() });
+    for (var i = 0; i < content.children.length; i++) {
+        var element = content.children[i];
+        if (headerTags.includes(element.tagName)) {
+            headers.push(elementToObject(element));
+        }
+    }
+    return headers;
 }
-getTOC();
+
+function elementToObject(element) {
+    var obj = {};
+    obj.id = element.id;
+    obj.textContent = element.textContent;
+    obj.tagName = element.tagName;
+    return obj;
+}
+
+getTableOfContents();
