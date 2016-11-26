9f3a2e5b76f91f4b67e414ff7c1e3324e22f68b0
Chris Li <chris@kiwix.org>
2016-11-26 17:12:02 -0500

    CoreDataTableBaseController comments

--- Kiwix-iOS/Controller/Others/CoreDataTableBaseController.swift
+++ Kiwix-iOS/Controller/Others/CoreDataTableBaseController.swift
@@ -12,7 +12,6 @@ import CoreData
 // Yes, I know it'd be better to make this into a protocol.
 // But unfortunately, UIKit cannot seems to be able to recognize default protocol implementation in Swift. (iOS 10.1)
 
-
 class CoreDataTableBaseController: UIViewController, NSFetchedResultsControllerDelegate {
     @IBOutlet weak var tableView: UITableView!
     
