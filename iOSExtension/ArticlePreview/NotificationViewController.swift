956818bc000dc867eb48b45b00b3d0e78022cce7
Chris Li <chrisshwli@gmail.com>
2017-02-01 11:38:26 -0500

    Add article preview notification extension

---
+++ iOSExtension/ArticlePreview/NotificationViewController.swift
@@ -0,0 +1,26 @@
+//
+//  NotificationViewController.swift
+//  ArticlePreview
+//
+//  Created by Chris Li on 2/1/17.
+//  Copyright © 2017 Chris Li. All rights reserved.
+//
+
+import UIKit
+import UserNotifications
+import UserNotificationsUI
+
+class NotificationViewController: UIViewController, UNNotificationContentExtension {
+
+    @IBOutlet var label: UILabel?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any required interface initialization here.
+    }
+    
+    func didReceive(_ notification: UNNotification) {
+        self.label?.text = notification.request.content.body
+    }
+
+}
