91e552d3fc9877bebac92dc8bd24ad2980d9ae0c
Chris Li <chrisshwli@gmail.com>
2017-02-12 18:32:27 -0500

    Adding macOS target

---
+++ Kiwix/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Kiwix
+//
+//  Created by Chris Li on 2/12/17.
+//  Copyright © 2017 Chris Li. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
