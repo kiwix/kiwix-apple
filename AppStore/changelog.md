16769fda5514247c52e2a7662a8160e4682ba405
Chris Li <chrisshwli@gmail.com>
2017-04-14 14:56:58 -0400

    release note for 1.8.1

---
+++ AppStore/changelog.md
@@ -0,0 +1,10 @@
+# 1.8
+We have improved Kiwix for you:
+- The library has a new look.
+- You can now use ZIM files that have a built-in index. 
+- Better performance.
+
+# 1.8.1
+- Clear search history & browsing history
+- Now remember scroll position when go back / forward when browsing
+- Fix: zim files no longer got backed up to iCloud ot iTunes
--- Shared/Operations/ScanProcedure.swift
+++ Shared/Operations/ScanProcedure.swift
@@ -119,11 +119,17 @@ class ScanLocalBookOperation: Procedure {
     }
     
     private func excludeDocDirFromBackup() {
-        var docDirURL = (try? FileManager.default.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: false))!
-        guard FileManager.default.fileExists(atPath: docDirURL.path) else {return}
-        var value = URLResourceValues()
-        value.isExcludedFromBackup = true
-        try? docDirURL.setResourceValues(value)
+        let docDirURL = (try? FileManager.default.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: false))!
+        let contents = (try? FileManager.default.contentsOfDirectory(at: docDirURL, includingPropertiesForKeys: nil, options: .skipsSubdirectoryDescendants)) ?? [URL]()
+        for var url in contents {
+            var value = URLResourceValues()
+            value.isExcludedFromBackup = true
+            try? url.setResourceValues(value)
+        }
+//        guard FileManager.default.fileExists(atPath: docDirURL.path) else {return}
+//        var value = URLResourceValues()
+//        value.isExcludedFromBackup = true
+//        try? docDirURL.setResourceValues(value)
     }
     
 }
