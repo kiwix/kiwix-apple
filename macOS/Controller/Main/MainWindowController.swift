5e60c964abc3b8a8c9aea6e2c61bf859d643d157
Chris Li <chrisshwli@gmail.com>
2017-08-25 17:09:42 -0400

    search field bug

--- macOS/Controller/Main/MainWindowController.swift
+++ macOS/Controller/Main/MainWindowController.swift
@@ -9,9 +9,11 @@
 import Cocoa
 import SwiftyUserDefaults
 
-class MainWindowController: NSWindowController {
+class MainWindowController: NSWindowController, NSSearchFieldDelegate {
     @IBOutlet weak var titleTextField: NSTextField!
     @IBOutlet weak var loadingView: NSProgressIndicator!
+    @IBOutlet weak var searchField: NSSearchField!
+    @IBOutlet weak var searchFieldItem: NSToolbarItem!
     
     override func windowDidLoad() {
         super.windowDidLoad()
@@ -54,6 +56,79 @@ class MainWindowController: NSWindowController {
             searchController.clearSearch()
             webController.loadMainPage()
         }
+    }
+    
+    // MARK: - NSSearchFieldDelegate
+    
+    let searchResultWindowController = SearchResultWindowController()
+    
+    override func controlTextDidBeginEditing(_ obj: Notification) {
+        guard let resultWindow = searchResultWindowController.window else {return}
+        if !resultWindow.isVisible {
+            showSearchResultWindow(searchField: searchField)
+        }
+    }
+    
+    override func controlTextDidEndEditing(_ obj: Notification) {
+    }
+    
+    func showSearchResultWindow(searchField: NSSearchField) {
+        guard let parentWindow = searchField.window, let parentView = searchField.superview,
+            let resultWindow = searchResultWindowController.window else {return}
+        
+        var frame = parentView.convert(searchField.frame, to: nil)
+        frame = parentWindow.convertToScreen(frame)
+        frame = NSRect(origin: frame.origin, size: CGSize(width: searchField.frame.width, height: 300))
+        frame = frame.offsetBy(dx: 0, dy: -2)
+        resultWindow.setFrame(frame, display: true)
+        resultWindow.setFrameTopLeftPoint(frame.origin)
+        parentWindow.addChildWindow(resultWindow, ordered: .above)
+    }
+}
+
+class SearchFieldItem: NSToolbarItem {
+    override var minSize: NSSize {
+        get {return NSSize(width: 0, height: 22)}
+        set {}
+    }
+    
+    override var maxSize: NSSize {
+        get {return NSSize(width: 600, height: 22)}
+        set {}
+    }
+}
+
+class SearchField: NSSearchField {
+    override var intrinsicContentSize: NSSize {
+        get {return NSSize(width: 600, height: 22)}
+        set {}
+    }
+    override var preferredMaxLayoutWidth: CGFloat {
+        get {return 600}
+        set {}
+    }
+}
+
+
+class SearchResultWindowController: NSWindowController {
+    init() {
+        let window = NSWindow()
+        window.styleMask = .borderless
+        window.contentView = RoundedCornerView()
+        print(window.contentView?.frame)
+        super.init(window: window)
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+}
+
+class RoundedCornerView: NSView {
+    override func draw(_ dirtyRect: NSRect) {
+        let borderPath = NSBezierPath(roundedRect: bounds, xRadius: 4, yRadius: 4)
+        NSColor.windowBackgroundColor.setFill()
+        borderPath.fill()
     }
 }
 
