d52216cb3e560e006bed9928183fb8850ced0fa2
Chris Li <chrisshwli@gmail.com>
2017-08-30 14:54:25 -0400

    add icon

--- macOS/View/SearchView.swift
+++ macOS/View/SearchView.swift
@@ -36,6 +36,8 @@ class SearchTitleResultTableCellView: NSTableCellView {
 
 class SearchFieldContainer: NSView {
     override func draw(_ dirtyRect: NSRect) {
+        /* We are putting NSSearhField into a plain NSView to get around the NSSearhField in toolbar resizing bug. By removing the view's mask, the search field woun't be clipped. Otherwise, the gray rounded rect border of the NSSearhField will be clipped.
+         */
         wantsLayer = true
         layer?.masksToBounds = false
     }
@@ -64,6 +66,8 @@ class SearchField: NSSearchField {
     let prompt = "Search"
     
     override func awakeFromNib() {
+        /* Override default cancel button behavior. By default, the search field loses focus when cancel button is clicked. However, in this case, we want the search text to be cleared and keep focus on search field.
+         */
         guard let cell = (cell as? NSSearchFieldCell)?.cancelButtonCell else {return}
         cell.target = self
         cell.action = #selector(cancelButtonClicked)
