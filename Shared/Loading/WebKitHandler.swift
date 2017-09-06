4c8d3fd7bebf0efb5f2e1e6af02e0aee6313c5f1
Chris Li <chrisshwli@gmail.com>
2017-09-06 16:37:20 -0400

    commit

---
+++ Shared/Loading/WebKitHandler.swift
@@ -0,0 +1,18 @@
+//
+//  KiwixURLSchemeHandler.swift
+//  WikiMed
+//
+//  Created by Chris Li on 9/6/17.
+//  Copyright © 2017 Chris Li. All rights reserved.
+//
+
+import WebKit
+
+class KiwixURLSchemeHandler: WKURLSchemeHandler {
+    func webView(_ webView: WKWebView, start urlSchemeTask: WKURLSchemeTask) {
+    }
+    
+    func webView(_ webView: WKWebView, stop urlSchemeTask: WKURLSchemeTask) {
+        
+    }
+}
