cda7d362073a4b43045f655413643fe7bc98705e
Chris Li <chrisshwli@gmail.com>
2017-09-06 15:41:13 -0400

    init wikimed

---
+++ WikiMed/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WikiMed
+//
+//  Created by Chris Li on 9/6/17.
+//  Copyright © 2017 Chris Li. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
