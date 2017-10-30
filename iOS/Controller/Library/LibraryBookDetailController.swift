55059a981bc9e27f2f81bf56bbfef287b19a20e7
Chris Li <chrisshwli@gmail.com>
2017-10-30 11:54:12 -0400

    restructure

--- iOS/Controller/Library/LibraryBookDetailController.swift
+++ iOS/Controller/Library/LibraryBookDetailController.swift
@@ -12,27 +12,32 @@ class LibraryBookDetailController: UIViewController, UITableViewDelegate, UITabl
     private(set) var book: Book?
     private(set) var observer: NSKeyValueObservation?
     let tableView = UITableView(frame: .zero, style: .grouped)
-    var actions = [[String]]()
-    let meta = [
-        [Titles.size, Titles.date],
-        [Titles.articleCount, Titles.mediaCount, Titles.globalCount]
+    
+    var actions = [[Action]]()
+    let metas: [[BookMeta]] = [
+        [.size, .date],
+        [.articleCount, .mediaCount, .globalCount]
     ]
     
-    struct Titles {
-        static let downloadWifiOnly = NSLocalizedString("Download - Wifi Only", comment: "Book Detail Cell")
-        static let downloadWifiAndCellular = NSLocalizedString("Download - Wifi & Cellular", comment: "Book Detail Cell")
-        static let cancel = NSLocalizedString("Cancel", comment: "Book Detail Cell")
-        static let resume = NSLocalizedString("Resume", comment: "Book Detail Cell")
-        static let pause = NSLocalizedString("Pause", comment: "Book Detail Cell")
-        static let deleteFile = NSLocalizedString("Delete File", comment: "Book Detail Cell")
-        static let deleteBookmarks = NSLocalizedString("Delete Bookmarks", comment: "Book Detail Cell")
-        static let deleteFileAndBookmarks = NSLocalizedString("Delete File and Bookmarks", comment: "Book Detail Cell")
-        static let openMainPage = NSLocalizedString("Open Main Page", comment: "Book Detail Cell")
-        static let size = NSLocalizedString("Size", comment: "Book Detail Cell")
-        static let date = NSLocalizedString("Date", comment: "Book Detail Cell")
-        static let articleCount = NSLocalizedString("Article Count", comment: "Book Detail Cell")
-        static let mediaCount = NSLocalizedString("Media Count", comment: "Book Detail Cell")
-        static let globalCount = NSLocalizedString("Global Count", comment: "Book Detail Cell")
+    enum Action {
+        case downloadWifiOnly, downloadWifiAndCellular, downloadSpaceNotEnough
+        case cancel, resume, pause
+        case deleteFile, deleteBookmarks, deleteFileAndBookmarks
+        case openMainPage
+        
+        static let destructives: [Action] = [.cancel, .deleteFile, .deleteBookmarks, .deleteFileAndBookmarks]
+        
+        var isDestructive: Bool {
+            return Action.destructives.contains(self)
+        }
+        
+        var isDisabled: Bool {
+            return self == .downloadSpaceNotEnough
+        }
+    }
+    
+    enum BookMeta: String {
+        case size, date, articleCount, mediaCount, globalCount
     }
     
     convenience init(book: Book) {
@@ -42,14 +47,13 @@ class LibraryBookDetailController: UIViewController, UITableViewDelegate, UITabl
             guard change.newValue != change.oldValue, let newValue = change.newValue, let state = BookState(rawValue: Int(newValue)) else {return}
             switch state {
             case .cloud:
-                self.actions = [[Titles.downloadWifiOnly, Titles.downloadWifiAndCellular]]
+                self.actions = [[.downloadWifiOnly, .downloadWifiAndCellular]]
             case .downloading:
-                self.actions = [[Titles.cancel, Titles.pause]]
+                self.actions = [[.cancel, .pause]]
             case .local:
-                self.actions = [[Titles.deleteFile, Titles.deleteBookmarks, Titles.deleteFileAndBookmarks],
-                           [Titles.openMainPage]]
+                self.actions = [[.deleteFile, .deleteBookmarks, .deleteFileAndBookmarks], [.openMainPage]]
             case .retained:
-                self.actions = [[Titles.deleteBookmarks]]
+                self.actions = [[.deleteBookmarks]]
             }
             self.tableView.reloadData()
         }
@@ -60,46 +64,68 @@ class LibraryBookDetailController: UIViewController, UITableViewDelegate, UITabl
         view = tableView
         tableView.delegate = self
         tableView.dataSource = self
-        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "ActionCell")
+        tableView.register(LibraryActionCell.self, forCellReuseIdentifier: "ActionCell")
     }
     
     // MARK: - UITableViewDataSource & Delagates
     
     func numberOfSections(in tableView: UITableView) -> Int {
-        return actions.count + meta.count
+        return actions.count + metas.count
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return section < actions.count ? actions[section].count : meta[section-actions.count].count
+        return section < actions.count ? actions[section].count : metas[section-actions.count].count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         if indexPath.section < actions.count {
-            let cell = tableView.dequeueReusableCell(withIdentifier: "ActionCell", for: indexPath)
-            let title = actions[indexPath.section][indexPath.row]
-            cell.textLabel?.text = title
-            cell.textLabel?.textAlignment = .center
-            cell.textLabel?.font = UIFont.systemFont(ofSize: 17, weight: .medium)
-            cell.textLabel?.textColor = [Titles.openMainPage, Titles.pause, Titles.resume].contains(title) ? #colorLiteral(red: 0, green: 0.5898008943, blue: 1, alpha: 1) : #colorLiteral(red: 1, green: 0.1491314173, blue: 0, alpha: 1)
+            let cell = tableView.dequeueReusableCell(withIdentifier: "ActionCell", for: indexPath) as! LibraryActionCell
+            let action = actions[indexPath.section][indexPath.row]
+            if action.isDestructive {cell.isDestructive = true}
+            if action.isDestructive {cell.isDestructive = true}
+            switch action {
+            case .downloadWifiOnly:
+                cell.textLabel?.text = NSLocalizedString("Download - Wifi Only", comment: "Book Detail Cell")
+            case .downloadWifiAndCellular:
+                cell.textLabel?.text = NSLocalizedString("Download - Wifi & Cellular", comment: "Book Detail Cell")
+            case .downloadSpaceNotEnough:
+                cell.textLabel?.text = NSLocalizedString("Download - Space Not Enough", comment: "Book Detail Cell")
+            case .cancel:
+                cell.textLabel?.text = NSLocalizedString("Cancel", comment: "Book Detail Cell")
+            case .resume:
+                cell.textLabel?.text = NSLocalizedString("Resume", comment: "Book Detail Cell")
+            case .pause:
+                cell.textLabel?.text = NSLocalizedString("Pause", comment: "Book Detail Cell")
+            case .deleteFile:
+                cell.textLabel?.text = NSLocalizedString("Delete File", comment: "Book Detail Cell")
+            case .deleteBookmarks:
+                cell.textLabel?.text = NSLocalizedString("Delete Bookmarks", comment: "Book Detail Cell")
+            case .deleteFileAndBookmarks:
+                cell.textLabel?.text = NSLocalizedString("Delete File and Bookmarks", comment: "Book Detail Cell")
+            case .openMainPage:
+                cell.textLabel?.text = NSLocalizedString("Open Main Page", comment: "Book Detail Cell")
+            }
             return cell
         } else {
             let cell = tableView.dequeueReusableCell(withIdentifier: "Cell") ?? UITableViewCell(style: UITableViewCellStyle.value1, reuseIdentifier: "Cell")
-            let title = meta[indexPath.section-actions.count][indexPath.row]
-            cell.textLabel?.text = title
+            let meta = metas[indexPath.section-actions.count][indexPath.row]
             cell.selectionStyle = .none
-            switch title {
-            case Titles.size:
+            switch meta {
+            case .size:
+                cell.textLabel?.text = NSLocalizedString("Size", comment: "Book Detail Cell")
                 cell.detailTextLabel?.text = book?.fileSizeDescription
-            case Titles.date:
+            case .date:
+                cell.textLabel?.text = NSLocalizedString("Date", comment: "Book Detail Cell")
                 cell.detailTextLabel?.text = book?.dateDescription
-            case Titles.articleCount:
+            case .articleCount:
+                cell.textLabel?.text = NSLocalizedString("Article Count", comment: "Book Detail Cell")
                 if let count = book?.articleCount {cell.detailTextLabel?.text = "\(count)"}
-            case Titles.mediaCount:
+            case .mediaCount:
+                cell.textLabel?.text = NSLocalizedString("Media Count", comment: "Book Detail Cell")
                 if let count = book?.mediaCount {cell.detailTextLabel?.text = "\(count)"}
-            case Titles.globalCount:
+            case .globalCount:
+                cell.textLabel?.text = NSLocalizedString("Global Count", comment: "Book Detail Cell")
                 if let count = book?.globalCount {cell.detailTextLabel?.text = "\(count)"}
-            default:
-                break
             }
             return cell
         }
@@ -109,9 +135,9 @@ class LibraryBookDetailController: UIViewController, UITableViewDelegate, UITabl
         tableView.deselectRow(at: indexPath, animated: true)
         guard let book = book else {return}
         if indexPath.section < actions.count {
-            let title = actions[indexPath.section][indexPath.row]
-            switch title {
-            case Titles.cancel:
+            let action = actions[indexPath.section][indexPath.row]
+            switch action {
+            case .cancel:
                 Network.shared.cancel(bookID: book.id)
             default:
                 break
