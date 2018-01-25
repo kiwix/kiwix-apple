85b7b041b0f7406a0654a820c3fe70971c3211c3
Chris Li <chrisshwli@gmail.com>
2018-01-25 17:34:50 -0500

    article cell

--- iOS/View/ArticleTableViewCell.swift
+++ iOS/View/ArticleTableViewCell.swift
@@ -12,13 +12,14 @@ class ArticleTableViewCell: UITableViewCell {
     let titleLabel = UILabel()
     let snippetLabel = UILabel()
     let thumbImageView = UIImageView()
-    private let textStackView = UIStackView()
+    let textStackView = UIStackView()
+    private var configuredConstraints = false
     
     override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
         configure()
     }
-    
+
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
         configure()
@@ -31,10 +32,29 @@ class ArticleTableViewCell: UITableViewCell {
         thumbImageView.image = nil
     }
     
+    override func updateConstraints() {
+        defer { super.updateConstraints() }
+        guard !configuredConstraints else { return }
+        
+        NSLayoutConstraint.activate([
+            thumbImageView.heightAnchor.constraint(equalToConstant: 34),
+            thumbImageView.widthAnchor.constraint(equalToConstant: 34),
+            thumbImageView.centerYAnchor.constraint(equalTo: contentView.readableContentGuide.centerYAnchor),
+            thumbImageView.leftAnchor.constraint(equalTo: contentView.readableContentGuide.leftAnchor),
+            textStackView.leftAnchor.constraint(equalTo: thumbImageView.rightAnchor, constant: 8),
+            textStackView.rightAnchor.constraint(equalTo: contentView.readableContentGuide.rightAnchor),
+            textStackView.topAnchor.constraint(equalTo: contentView.readableContentGuide.topAnchor, constant: 6),
+            textStackView.bottomAnchor.constraint(equalTo: contentView.readableContentGuide.bottomAnchor, constant: -6)])
+        
+        configuredConstraints = true
+    }
+    
     func configure() {
         backgroundColor = .clear
         titleLabel.setContentHuggingPriority(.defaultLow, for: .vertical)
         snippetLabel.setContentHuggingPriority(.defaultHigh, for: .vertical)
+        titleLabel.setContentCompressionResistancePriority(.defaultHigh, for: .vertical)
+        snippetLabel.setContentCompressionResistancePriority(.defaultLow, for: .vertical)
         snippetLabel.numberOfLines = 0
         snippetLabel.font = UIFont.systemFont(ofSize: 12, weight: UIFont.Weight.regular)
         thumbImageView.clipsToBounds = true
@@ -50,14 +70,6 @@ class ArticleTableViewCell: UITableViewCell {
             $0.translatesAutoresizingMaskIntoConstraints = false
             contentView.addSubview($0)
         })
-        NSLayoutConstraint.activate([
-            thumbImageView.heightAnchor.constraint(equalToConstant: 34),
-            thumbImageView.widthAnchor.constraint(equalToConstant: 34),
-            thumbImageView.centerYAnchor.constraint(equalTo: contentView.readableContentGuide.centerYAnchor),
-            thumbImageView.leftAnchor.constraint(equalTo: contentView.readableContentGuide.leftAnchor),
-            textStackView.leftAnchor.constraint(equalTo: thumbImageView.rightAnchor, constant: 8),
-            textStackView.rightAnchor.constraint(equalTo: contentView.readableContentGuide.rightAnchor),
-            textStackView.topAnchor.constraint(equalTo: contentView.readableContentGuide.topAnchor, constant: 6),
-            textStackView.bottomAnchor.constraint(equalTo: contentView.readableContentGuide.bottomAnchor, constant: -6)])
+        setNeedsUpdateConstraints()
     }
 }
