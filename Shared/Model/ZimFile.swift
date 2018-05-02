7028f01c9c7ddf66b0b61864b8389c2d4afb5fb0
Chris Li <chrisshwli@gmail.com>
2018-05-02 14:16:42 -0400

    zimfile categories

--- Shared/Model/ZimFile.swift
+++ Shared/Model/ZimFile.swift
@@ -102,7 +102,7 @@ class ZimFile: Object {
         }
     }
     
-    enum Category: String {
+    enum Category: String, CustomStringConvertible {
         case wikibooks
         case wikinews
         case wikipedia
@@ -118,6 +118,68 @@ class ZimFile: Object {
         case stackExchange
         
         case other
+        
+        var description: String {
+            switch self {
+            case .wikibooks:
+                return NSLocalizedString("Wikibooks", comment: "Zim File Category")
+            case .wikinews:
+                return NSLocalizedString("Wikinews", comment: "Zim File Category")
+            case .wikipedia:
+                return NSLocalizedString("Wikipedia", comment: "Zim File Category")
+            case .wikiquote:
+                return NSLocalizedString("Wikiquote", comment: "Zim File Category")
+            case .wikisource:
+                return NSLocalizedString("Wikisource", comment: "Zim File Category")
+            case .wikispecies:
+                return NSLocalizedString("Wikispecies", comment: "Zim File Category")
+            case .wikiversity:
+                return NSLocalizedString("Wikiversity", comment: "Zim File Category")
+            case .wikivoyage:
+                return NSLocalizedString("Wikivoyage", comment: "Zim File Category")
+            case .wiktionary:
+                return NSLocalizedString("Wiktionary", comment: "Zim File Category")
+            case .ted:
+                return NSLocalizedString("TED", comment: "Zim File Category")
+            case .vikidia:
+                return NSLocalizedString("Vikidia", comment: "Zim File Category")
+            case .stackExchange:
+                return NSLocalizedString("StackExchange", comment: "Zim File Category")
+            case .other:
+                return NSLocalizedString("Other", comment: "Zim File Category")
+            }
+        }
+        
+        var icon: UIImage {
+            switch self {
+            case .wikibooks:
+                return #imageLiteral(resourceName: "Wikibooks")
+            case .wikinews:
+                return #imageLiteral(resourceName: "Wikinews")
+            case .wikipedia:
+                return #imageLiteral(resourceName: "Wikipedia")
+            case .wikiquote:
+                return #imageLiteral(resourceName: "Book")
+            case .wikisource:
+                return #imageLiteral(resourceName: "Book")
+            case .wikispecies:
+                return #imageLiteral(resourceName: "Wikispecies")
+            case .wikiversity:
+                return #imageLiteral(resourceName: "Wikiversity")
+            case .wikivoyage:
+                return #imageLiteral(resourceName: "Wikivoyage")
+            case .wiktionary:
+                return #imageLiteral(resourceName: "Book")
+            case .ted:
+                return #imageLiteral(resourceName: "Vikidia")
+            case .vikidia:
+                return #imageLiteral(resourceName: "Vikidia")
+            case .stackExchange:
+                return #imageLiteral(resourceName: "StackExchange")
+            case .other:
+                return #imageLiteral(resourceName: "Book")
+            }
+        }
     }
     
     class NumberAbbrevationFormatter {
