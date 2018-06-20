86109675479544a4ade2904f9003d0a87240200b
Chris Li <chrisshwli@gmail.com>
2018-06-20 17:02:12 -0400

    move settings

---
+++ Shared/Settings.swift
@@ -0,0 +1,13 @@
+//
+//  Settings.swift
+//  iOS
+//
+//  Created by Chris Li on 6/20/18.
+//  Copyright © 2018 Chris Li. All rights reserved.
+//
+
+import SwiftyUserDefaults
+
+extension DefaultsKeys {
+    static let searchResultExcludeSnippet = DefaultsKey<Bool>("searchResultExcludeSnippet")
+}
