e27d2643b0f10676abbbc555132a64fb877f2148
Chris Li <chrisshwli@gmail.com>
2018-08-08 14:21:10 -0400

    Wikimed template

---
+++ WikiMed/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WikiMed
+//
+//  Created by Chris Li on 7/30/18.
+//  Copyright © 2018 Chris Li. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
