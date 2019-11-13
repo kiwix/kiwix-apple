9f863cb67f89d463d5609b701dddb68cef28bbd9
ChrisLi <chris.li@datarobot.com>
2019-11-12 23:24:56 -0500

    search controller

--- macOS/Controller/SearchController.swift
+++ macOS/Controller/SearchController.swift
@@ -15,6 +15,42 @@ class SearchWindow: NSWindow {
 }
 
 
-class SearchController: NSViewController {
+class SearchController: NSViewController, SearchQueueEvents {
+    @IBOutlet weak var tabView: NSTabView!
+    @IBOutlet weak var resultsOutlineView: NSOutlineView!
+    @IBOutlet weak var progressIndicator: NSProgressIndicator!
     
+    private let queue = SearchQueue()
+    private(set) var searchText: String = ""
+    private var results = [SearchResult]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        queue.eventDelegate = self
+    }
+    
+    // MARK: SearchQueueEvents
+    
+    func searchStarted() {
+        tabView.selectTabViewItem(withIdentifier: Mode.inProgress.rawValue)
+        progressIndicator.startAnimation(nil)
+    }
+    
+    func searchFinished(searchText: String, results: [SearchResult]) {
+        self.searchText = searchText
+        self.results = results
+        resultsOutlineView.reloadData()
+        progressIndicator.stopAnimation(nil)
+        if results.count > 0 {
+            tabView.selectTabViewItem(withIdentifier: Mode.results.rawValue)
+        } else {
+            tabView.selectTabViewItem(withIdentifier: Mode.noResult.rawValue)
+        }
+    }
+}
+
+private enum Mode: String {
+    case results = "Results"
+    case inProgress = "InProgress"
+    case noResult = "NoResult"
 }
