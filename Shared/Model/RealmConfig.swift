4178ff6ef1f7018148c41432903a3e774d780480
Chris Li <chris@kiwix.org>
2019-11-27 18:23:13 -0500

    realm location

--- Shared/Model/RealmConfig.swift
+++ Shared/Model/RealmConfig.swift
@@ -16,12 +16,14 @@ extension Realm {
     
     static let defaultConfig: Realm.Configuration = {
         let library = try! FileManager.default.url(for: .libraryDirectory, in: .userDomainMask, appropriateFor: nil, create: false)
-        let applicationSupport = try! FileManager.default.url(for: .applicationSupportDirectory, in: .userDomainMask, appropriateFor: nil, create: false)
+        let applicationSupport = try! FileManager.default.url(for: .applicationSupportDirectory, in: .userDomainMask, appropriateFor: nil, create: true)
         let oldDatabaseURL = library.appendingPathComponent("realm")
         let newDatabaseURL = applicationSupport.appendingPathComponent("kiwix.realm")
         
-        // move database to new location
-        try? FileManager.default.moveItem(at: oldDatabaseURL, to: newDatabaseURL)
+        // move database to application support
+        if FileManager.default.fileExists(atPath: oldDatabaseURL.path) {
+            try? FileManager.default.moveItem(at: oldDatabaseURL, to: newDatabaseURL)
+        }
         
         var config = Realm.Configuration(schemaVersion: 1)
         config.fileURL = newDatabaseURL
