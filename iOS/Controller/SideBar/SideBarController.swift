2c87df5affeeaa2224af647df1c7eaca85d5167d
Chris Li <chris@kiwix.org>
2019-11-30 09:31:34 -0500

    remove session

--- iOS/Controller/SideBar/SideBarController.swift
+++ iOS/Controller/SideBar/SideBarController.swift
@@ -13,7 +13,6 @@ class SideBarController: UITabBarController {
     init() {
         super.init(nibName: nil, bundle: nil)
         viewControllers = [
-            UINavigationController(rootViewController: SessionController()),
             UINavigationController(rootViewController: FavoriteController()),
             UINavigationController(rootViewController: MapController()),
         ]
@@ -21,24 +20,6 @@ class SideBarController: UITabBarController {
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
-    }
-}
-
-@available(iOS 13.0, *)
-class SessionController: UITableViewController {
-    init() {
-        super.init(nibName: nil, bundle: nil)
-        tabBarItem = UITabBarItem(title: "Session", image: UIImage(systemName: "desktopcomputer"), tag: 0)
-    }
-    
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        title = "Session"
-        navigationController?.navigationBar.prefersLargeTitles = true
     }
 }
 
