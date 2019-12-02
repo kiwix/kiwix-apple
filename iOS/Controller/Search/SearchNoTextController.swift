e352f32ec674991dcd1f1fe08c5b9e6cd48ee67b
Chris Li <chris@kiwix.org>
2019-12-01 23:21:41 -0500

    connect recent search

--- iOS/Controller/Search/SearchNoTextController.swift
+++ iOS/Controller/Search/SearchNoTextController.swift
@@ -244,7 +244,9 @@ class SearchNoTextController: UIViewController, UITableViewDelegate, UITableView
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         collectionView.deselectItem(at: indexPath, animated: true)
         let searchText = recentSearchTexts[indexPath.row]
-        if let main = presentingViewController as? MainController {
+        if #available(iOS 13.0, *), let controller = presentingViewController as? ContentViewController {
+            controller.searchController.searchBar.text = searchText
+        } else if let main = presentingViewController as? MainController {
             main.searchController.searchBar.text = searchText
         }
     }
