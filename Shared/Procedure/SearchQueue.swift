755e1bb128a9bf8ccd41cf9c5852b932aa935b12
ChrisLi <chris@kiwix.org>
2020-02-09 22:44:29 -0500

    macOS 2.1 build2 (#218)
    * search queue
    
    * remove old code
    
    * remove code
    
    * build number
    
    * build bump

--- Shared/Procedure/SearchQueue.swift
+++ Shared/Procedure/SearchQueue.swift
@@ -9,42 +9,8 @@
 import SwiftyUserDefaults
 
 class SearchQueue: OperationQueue {
-    weak var eventDelegate: SearchQueueEvents?
-    private var operationsObserver: NSKeyValueObservation?
-    
     override init() {
         super.init()
         maxConcurrentOperationCount = 1
-        operationsObserver = observe(\.operations, options: [.new, .old], changeHandler: { (operation, change) in
-            guard let oldOperations = change.oldValue, let newOperations = change.newValue else {return}
-            if (newOperations.count > oldOperations.count) {
-                // did start search
-                guard oldOperations.count == 0 else {return}
-                DispatchQueue.main.async {
-                    self.eventDelegate?.searchStarted()
-                }
-            } else {
-                // did finish search operation
-                guard newOperations.count == 0, let operation = oldOperations.last as? SearchProcedure else {return}
-                DispatchQueue.main.async {
-                    if operation.isCancelled {
-                        self.eventDelegate?.searchFinished(searchText: "", results: [], isCancelled: true)
-                    } else {
-                        self.eventDelegate?.searchFinished(searchText: operation.searchText, results: operation.sortedResults, isCancelled: false)
-                    }
-                }
-            }
-        })
     }
-    
-    func enqueue(searchText: String, zimFileIDs: Set<ZimFileID>) {
-        cancelAllOperations()
-        let procedure = SearchProcedure(term: searchText, ids: zimFileIDs, extractSnippet: !Defaults[.searchResultExcludeSnippet])
-        addOperation(procedure)
-    }
-}
-
-protocol SearchQueueEvents: class {
-    func searchStarted()
-    func searchFinished(searchText: String, results: [SearchResult], isCancelled: Bool)
 }
