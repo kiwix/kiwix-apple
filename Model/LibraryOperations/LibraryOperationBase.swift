a735f5398f1719fa6d4e68ea6008d143f296aac9
ChrisLi <chris.li@datarobot.com>
2020-05-08 18:03:59 -0400

    fix: favicon would disappear for existing zim files after library manual refresh (#246)
    * only update optional property if there is a new value
    
    * changelog

--- Model/LibraryOperations/LibraryOperationBase.swift
+++ Model/LibraryOperations/LibraryOperationBase.swift
@@ -29,16 +29,16 @@ class LibraryOperationBase: Operation {
         zimFile.fileDescription = meta.fileDescription
         zimFile.languageCode = meta.languageCode
         zimFile.categoryRaw = meta.category
-
-        zimFile.creator = meta.creator
-        zimFile.publisher = meta.publisher
-        zimFile.creationDate = meta.creationDate
-        zimFile.downloadURL = meta.downloadURL?.absoluteString
-        zimFile.faviconURL = meta.faviconURL?.absoluteString
-        zimFile.faviconData = meta.faviconData
-        zimFile.size.value = meta.size?.int64Value
-        zimFile.articleCount.value = meta.articleCount?.int64Value
-        zimFile.mediaCount.value = meta.mediaCount?.int64Value
+        
+        if let creator = meta.creator { zimFile.creator = creator}
+        if let publisher = meta.publisher { zimFile.publisher = publisher }
+        if let creationDate = meta.creationDate { zimFile.creationDate = creationDate }
+        if let url = meta.downloadURL { zimFile.downloadURL = url.absoluteString }
+        if let url = meta.faviconURL { zimFile.faviconURL = url.absoluteString }
+        if let faviconData = meta.faviconData { zimFile.faviconData = faviconData }
+        if let size = meta.size { zimFile.size.value = size.int64Value }
+        if let articleCount = meta.articleCount { zimFile.articleCount.value = articleCount.int64Value }
+        if let mediaCount = meta.mediaCount { zimFile.mediaCount.value = mediaCount.int64Value }
 
         zimFile.hasDetails = meta.hasDetails
         zimFile.hasIndex = meta.hasIndex
