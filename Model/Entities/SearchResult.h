7fee7d55f841d85f59bce7af4beee9522d76492a
ChrisLi <chris.li@datarobot.com>
2020-05-09 17:24:24 -0400

    project setup

---
+++ Model/Entities/SearchResult.h
@@ -0,0 +1,17 @@
+//
+//  SearchResult.h
+//  Kiwix
+//
+//  Created by Chris Li on 5/9/20.
+//  Copyright © 2020 Chris Li. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SearchResult : NSObject
+
+@property (nonatomic, strong, nonnull) NSString *zimFileID;
+@property (nonatomic, strong, nonnull) NSString *title;
+@property (nonatomic, strong, nullable) NSString *snippet;
+
+@end
---
+++ Model/Entities/SearchResult.m
@@ -0,0 +1,13 @@
+//
+//  SearchResult.m
+//  iOS
+//
+//  Created by Chris Li on 5/9/20.
+//  Copyright © 2020 Chris Li. All rights reserved.
+//
+
+#import "SearchResult.h"
+
+@implementation SearchResult
+
+@end
