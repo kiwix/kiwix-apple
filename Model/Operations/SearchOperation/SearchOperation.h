b1cfaf96c416fa50f4d902a44ecf4b2ab101bf32
ChrisLi <chris.li@datarobot.com>
2020-05-09 17:18:01 -0400

    search operation

---
+++ Model/Operations/SearchOperation/SearchOperation.h
@@ -0,0 +1,21 @@
+//
+//  SearchOperation.h
+//  Kiwix
+//
+//  Created by Chris Li on 5/9/20.
+//  Copyright © 2020 Chris Li. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface SearchOperation : NSOperation
+
+@property (nonatomic, strong, nonnull) NSArray *results;
+
+- (id)initWithSearchText:(NSString *)searchText zimFileIDs:(NSSet *)identifiers;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Model/Operations/SearchOperation/SearchOperation.mm
@@ -0,0 +1,71 @@
+//
+//  SearchOperation.mm
+//  Kiwix
+//
+//  Created by Chris Li on 5/9/20.
+//  Copyright © 2020 Chris Li. All rights reserved.
+//
+
+#import "SearchOperation.h"
+#import "ZimMultiReader.h"
+#include "reader.h"
+#include "searcher.h"
+
+struct SharedReaders {
+    NSArray *readerIDs;
+    std::vector<std::shared_ptr<kiwix::Reader>> readers;
+};
+
+@interface SearchOperation ()
+
+@property (strong) NSString *searchText;
+@property (strong) NSSet *identifiers;
+
+@end
+
+@implementation SearchOperation
+
+- (id)initWithSearchText:(NSString *)searchText zimFileIDs:(NSSet *)identifiers {
+    self = [super init];
+    if (self) {
+        self.searchText = searchText;
+        self.identifiers = identifiers;
+        self.results = @[];
+    }
+    return self;
+}
+
+- (void)main {
+    struct SharedReaders sharedReaders = [[ZimMultiReader shared] getSharedReaders:self.identifiers];
+    NSMutableArray *results = [[NSMutableArray alloc] initWithCapacity:20];
+    
+    // initialize kiwix::Search
+    kiwix::Searcher searcher = kiwix::Searcher();
+    for (auto iter: sharedReaders.readers) {
+        searcher.add_reader(iter.get());
+    }
+    
+    // start search
+    if (self.isCancelled) { return; }
+    searcher.search([self.searchText cStringUsingEncoding:NSUTF8StringEncoding], 0, 20);
+    
+    // retrieve search results
+    kiwix::Result *result = searcher.getNextResult();
+    while (result != NULL) {
+        NSString *identifier = sharedReaders.readerIDs[result->get_readerIndex()];
+        NSString *title = [NSString stringWithCString:result->get_title().c_str() encoding:NSUTF8StringEncoding];
+        NSString *path = [NSString stringWithCString:result->get_url().c_str() encoding:NSUTF8StringEncoding];
+        NSNumber *probability = [[NSNumber alloc] initWithDouble:(double)result->get_score() / double(100)];
+        [results addObject:@{
+            @"title": title
+        }];
+        
+        delete result;
+        result = searcher.getNextResult();
+        if (self.isCancelled) { break; }
+    }
+    
+    self.results = results;
+}
+
+@end
