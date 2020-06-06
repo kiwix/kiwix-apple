d265fc439a7e2b0f55ba7f7288328f0a8c409cdf
Chris Li <chris@kiwix.org>
2020-06-06 16:47:29 -0400

    zimFilesCancellable

--- iOS/Controller/Search/SearchFilterController.swift
+++ iOS/Controller/Search/SearchFilterController.swift
@@ -14,15 +14,15 @@ import RealmSwift
 
 @available(iOS 13.0, *)
 fileprivate struct ZimFileView: View {
-    let zimFile: ZimFileData
+    let zimFile: ZimFile
     
-    init(_ zimFile: ZimFileData) {
+    init(_ zimFile: ZimFile) {
         self.zimFile = zimFile
     }
     
     var body: some View {
         HStack {
-            Image(uiImage: UIImage(data: zimFile.faviconData) ?? #imageLiteral(resourceName: "GenericZimFile"))
+            Image(uiImage: UIImage(data: zimFile.faviconData ?? Data()) ?? #imageLiteral(resourceName: "GenericZimFile"))
                 .resizable()
                 .aspectRatio(contentMode: .fit)
                 .frame(width: 32.0, height: 32.0)
@@ -30,12 +30,8 @@ fileprivate struct ZimFileView: View {
                 Text(zimFile.title)
                     .font(.body)
                     .lineLimit(1)
-                Text(zimFile.detail)
+                Text(zimFile.description)
                     .font(.footnote)
-//                Text([
-//                    zimFile.sizeDescription, zimFile.creationDateDescription, zimFile.articleCountDescription
-//                ].compactMap({ $0 }).joined(separator: ", "))
-//                    .font(.footnote)
             }
             Spacer()
             if zimFile.includedInSearch {
@@ -73,8 +69,10 @@ struct SearchFilterView: View {
             }
             Section(header: Text("Files")) {
                 ForEach(viewModel.zimFiles, id: \.id) { data in
-                    ZimFileView(data).onTapGesture {
-                        self.viewModel.toggleZimFileIncludedInSearch(zimFileID: data.id)
+                    ZimFileView(data)
+                        .contentShape(Rectangle())
+                        .onTapGesture {
+                            self.viewModel.toggleZimFileIncludedInSearch(zimFileID: data.id)
                     }
                 }
             }
@@ -85,34 +83,25 @@ struct SearchFilterView: View {
 // MARK: - View Model
 
 @available(iOS 13.0, *)
-fileprivate struct ZimFileData {
-    var id: String
-    @State var title: String
-    @State var detail: String
-    @State var faviconData: Data
-    @State var includedInSearch: Bool
-}
-
-@available(iOS 13.0, *)
 fileprivate class ViewModel: ObservableObject {
     private let database = try? Realm(configuration: Realm.defaultConfig)
-    var t: AnyCancellable?
+    @Published private(set) var recentSearchTexts = [String]()
+    @Published private(set) var zimFiles = [ZimFile]()
+    
+    var zimFilesCancellable: AnyCancellable?
+    
     init() {
-        do {
-            let database = try Realm(configuration: Realm.defaultConfig)
-            let predicate = NSPredicate(format: "stateRaw == %@", ZimFile.State.onDevice.rawValue)
-            let zimFiles = database.objects(ZimFile.self).filter(predicate).publisher
-            t = zimFiles.map { (results) -> [ZimFileData] in
-                results.map({ ZimFileData(id: $0.id, title: $0.title, detail: "test", faviconData: $0.faviconData ?? Data(), includedInSearch: $0.includedInSearch) })
+        let predicate = NSPredicate(format: "stateRaw == %@", ZimFile.State.onDevice.rawValue)
+        zimFilesCancellable = database?.objects(ZimFile.self)
+            .filter(predicate)
+            .publisher
+            .map { results -> [ZimFile] in
+                results.map({ $0 })
             }.catch { error in
-                Just([ZimFileData]())
+                Just([ZimFile]())
             }.assign(to: \.zimFiles, on: self)
-        } catch {}
         recentSearchTexts = Defaults[.recentSearchTexts]
     }
-
-    @Published private(set) var recentSearchTexts = [String]()
-    @Published private(set) var zimFiles = [ZimFileData]()
     
     func toggleZimFileIncludedInSearch(zimFileID: String) {
         guard let zimFile = database?.object(ofType: ZimFile.self, forPrimaryKey: zimFileID) else { return }
