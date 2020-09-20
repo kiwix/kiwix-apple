9fc6b19673b90305d88e4d5a13883ed736e63ec2
ChrisLi <chris@kiwix.org>
2020-09-20 12:15:47 -0400

    ZimFileCell

--- iOS/View/ZimFileCell.swift
+++ iOS/View/ZimFileCell.swift
@@ -8,19 +8,43 @@
 
 import SwiftUI
 
-@available(iOS 13.0, *)
+@available(iOS 14.0, *)
 struct ZimFileCell: View {
+    let id: Int
+    
     var body: some View {
-        VStack {
-            Text("test")
-            Divider()
-        }
+        HStack(alignment: .center, spacing: 6) {
+            RoundedRectangle(cornerRadius: 4, style: .continuous)
+                .frame(width: 36, height: 36)
+            VStack(alignment: .leading, spacing: 6) {
+                VStack(alignment: .leading, spacing: 2) {
+                    Text("Row \(id)").font(.headline)
+                    Text("file_name_looooooong.zim")
+                        .font(.subheadline)
+                }
+                Divider()
+                HStack {
+                    Label("35.5 GB", systemImage: "internaldrive").font(.caption)
+                    Spacer()
+                    Label("3.5k articles", systemImage: "doc.text").font(.caption)
+                    Spacer()
+                    Label("2020-09-18", systemImage: "calendar").font(.caption)
+                }
+            }
+        }.padding(EdgeInsets(top: 5, leading: 10, bottom: 5, trailing: 10))
     }
 }
 
-@available(iOS 13.0, *)
+@available(iOS 14.0, *)
 struct ZimFileCell_Previews: PreviewProvider {
     static var previews: some View {
-        ZimFileCell().previewDevice(PreviewDevice(rawValue: "iPhone SE"))
+        ScrollView {
+            LazyVStack(alignment: .leading) {
+                ForEach(1...100, id: \.self) {
+                    ZimFileCell(id: $0)
+                }
+            }
+        }
+        .previewDevice(PreviewDevice(rawValue: "iPhone 11"))
     }
 }
