aa48fd7a2ebb8d7b219b4296e2056a95768ef8ae
ChrisLi <chris@kiwix.org>
2020-10-24 17:17:09 -0400

    revert

--- iOS/Controller/Web/WebKitWebController.swift
+++ iOS/Controller/Web/WebKitWebController.swift
@@ -20,25 +20,18 @@ class WebViewController: UIViewController, WKUIDelegate, WKNavigationDelegate {
     }()
     weak var delegate: WebViewControllerDelegate?
     
-    init() {
-        super.init(nibName: nil, bundle: nil)
-        webView.uiDelegate = self
-        webView.navigationDelegate = self
-        webView.allowsLinkPreview = true
-        webView.allowsBackForwardNavigationGestures = true
-    }
-    
     convenience init(url: URL) {
         self.init()
         load(url: url)
     }
     
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-    
     override func loadView() {
         view = webView
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureWebView()
     }
     
     var canGoBack: Bool {
@@ -59,6 +52,15 @@ class WebViewController: UIViewController, WKUIDelegate, WKNavigationDelegate {
         } else {
             return nil
         }
+    }
+    
+    // MARK: - Configure
+    
+    private func configureWebView() {
+        webView.uiDelegate = self
+        webView.navigationDelegate = self
+        webView.allowsLinkPreview = true
+        webView.allowsBackForwardNavigationGestures = true
     }
     
     // MARK: - loading
