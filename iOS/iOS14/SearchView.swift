15b5ebbbbcc5ae76f878e5d9bd7f8ca7f9a5a667
ChrisLi <chris@kiwix.org>
2020-11-03 22:04:28 -0500

    recent search buttons

--- iOS/iOS14/SearchView.swift
+++ iOS/iOS14/SearchView.swift
@@ -57,19 +57,30 @@ struct SearchView: View {
     }
     
     private var filter: some View {
-        LazyVStack {
-            Section(header: HStack {
-                Text("Search Filter").font(.title3).fontWeight(.semibold)
-                Spacer()
-            }.padding(.leading, 10)) {
-                ForEach(zimFilesViewModel.onDevice, id: \.id) { zimFile in
-                    ZimFileCell(zimFile, withIncludedInSearchIcon: true) {
-                        zimFilesViewModel.toggleIncludedInSearch(zimFileID: zimFile.id)
+        GeometryReader { geometry in
+            let horizontalPadding: CGFloat = geometry.size.width > 400 ? 20 : 16
+            ScrollView {
+                LazyVStack {
+                    Section(header: SectionHeader(text: "Recent Search").padding(.horizontal, horizontalPadding)) {
+                        ScrollView(.horizontal, showsIndicators: false) {
+                            LazyHStack {
+                                ForEach(0..<20) { index in
+                                    RecentSearchButton(text: "test \(Int.random(in: 0...100))")
+                                }
+                            }.padding(.horizontal, horizontalPadding)
+                        }.padding(.top, -4)
                     }
-                }
+                    Section(header: SectionHeader(text: "Search Filter")
+                    ) {
+                        ForEach(zimFilesViewModel.onDevice, id: \.id) { zimFile in
+                            ZimFileCell(zimFile, withIncludedInSearchIcon: true) {
+                                zimFilesViewModel.toggleIncludedInSearch(zimFileID: zimFile.id)
+                            }
+                        }
+                    }.padding(.horizontal, horizontalPadding)
+                }.padding(.vertical, 16)
             }
         }
-        .modifier(ScrollableModifier())
         .background(Color(.systemGroupedBackground).edgesIgnoringSafeArea(.all))
     }
     
@@ -108,7 +119,7 @@ struct SearchView: View {
                 } label: {
                     HStack(alignment: result.snippet == nil ? .center : .top) {
                         Favicon(zimFile: zimFilesViewModel.onDevice.first(where: {$0.id == result.zimFileID}))
-                        VStack(alignment: .leading) {
+                        VStack(alignment: .leading, spacing: 2) {
                             Text(result.title).font(.headline).lineLimit(1)
                             if let snippet = result.snippet {
                                 Text(snippet.string).font(.footnote).lineLimit(4)
@@ -136,5 +147,36 @@ struct SearchView: View {
         }
         .redacted(reason: .placeholder)
         .disabled(true)
+    }
+}
+
+@available(iOS 14.0, *)
+fileprivate struct SectionHeader : View {
+    let text: String
+    
+    var body: some View {
+        HStack {
+            Text(text).font(.title3).fontWeight(.semibold)
+            Spacer()
+        }.padding(.leading, 10)
+    }
+}
+
+@available(iOS 14.0, *)
+fileprivate struct RecentSearchButton : View {
+    let text: String
+    
+    var body: some View {
+        Button  {
+            
+        } label: {
+            Text(text)
+                .font(.footnote)
+                .fontWeight(.medium)
+                .foregroundColor(Color(.white))
+                .padding(.vertical, 4)
+                .padding(.horizontal, 10)
+                .background(RoundedRectangle(cornerRadius: 10, style: .continuous))
+        }
     }
 }
