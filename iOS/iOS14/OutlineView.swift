41cb2e527e00b7568cf88c51f9fb66c723ae0308
Chris Li <chris@kiwix.org>
2020-11-22 17:42:57 -0500

    unavailable outline

--- iOS/iOS14/OutlineView.swift
+++ iOS/iOS14/OutlineView.swift
@@ -14,26 +14,40 @@ struct OutlineView: View {
     @Binding var isSheetPresented: Bool
     
     var body: some View {
-        if let outlineItems = sceneViewModel.currentArticleOutlineItems {
-            List(outlineItems, id: \.index) { outlineItem in
-                Button {
-                    sceneViewModel.navigateToOutlineItem(index: outlineItem.index)
-                    if sceneViewModel.shouldAutoHideSidebar { sceneViewModel.hideSidebar() }
-                    isSheetPresented = false
-                } label: {
-                    if outlineItem.level == 1 {
-                        HStack {
-                            Spacer()
-                            Text(outlineItem.text).bold()
-                            Spacer()
+        switch sceneViewModel.contentDisplayMode {
+        case .web:
+            if let outlineItems = sceneViewModel.currentArticleOutlineItems, !outlineItems.isEmpty {
+                List(outlineItems, id: \.index) { outlineItem in
+                    Button {
+                        sceneViewModel.navigateToOutlineItem(index: outlineItem.index)
+                        if sceneViewModel.shouldAutoHideSidebar { sceneViewModel.hideSidebar() }
+                        isSheetPresented = false
+                    } label: {
+                        if outlineItem.level == 1 {
+                            HStack {
+                                Spacer()
+                                Text(outlineItem.text).bold()
+                                Spacer()
+                            }
+                        } else {
+                            Text(outlineItem.text).padding(.leading, 20 * CGFloat(outlineItem.level - 2))
                         }
-                    } else {
-                        Text(outlineItem.text).padding(.leading, 20 * CGFloat(outlineItem.level - 2))
                     }
                 }
+            } else {
+                unavailable
             }
-        } else {
-            Text("No Outline")
+        case .home:
+            unavailable
+        case .transition:
+            Color(.systemBackground)
+        }
+    }
+    
+    var unavailable: some View {
+        VStack(spacing: 20) {
+            Image("Compass").resizable().frame(width: 100, height: 100)
+            Text("Outline not available.").font(.headline).fontWeight(.semibold)
         }
     }
 }
