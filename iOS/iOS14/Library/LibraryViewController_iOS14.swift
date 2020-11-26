506acfc48a7537076a23aea15117181ed5d4088d
Chris Li <chris@kiwix.org>
2020-11-26 11:44:16 -0500

    basic categories

--- iOS/iOS14/Library/LibraryViewController_iOS14.swift
+++ iOS/iOS14/Library/LibraryViewController_iOS14.swift
@@ -31,13 +31,13 @@ struct LibraryView: View {
         NavigationView {
             ScrollView {
                 LazyVGrid(columns: [GridItem(.adaptive(minimum: 300))], spacing: 10) {
-                    Section(header: HStack {
-                        Text("On Device").font(.title2).fontWeight(.bold)
-                        Spacer()
-                    }.padding(.leading, 10)) {
-                        ForEach(libraryViewModel.onDevice, id: \.id) { zimFile in
-                            ZimFileCell(zimFile) {
-//                                sceneViewModel.loadMainPage(zimFile: zimFile)
+                    ForEach(libraryViewModel.result.categories, id: \.rawValue.hash) { category in
+                        Section(header: HStack {
+                            Text(category.description).font(.title2).fontWeight(.bold)
+                            Spacer()
+                        }) {
+                            ForEach(libraryViewModel.result.metaData[category, default: []]) { zimFile in
+                                ZimFileCell(zimFile) {}
                             }
                         }
                     }
@@ -45,45 +45,51 @@ struct LibraryView: View {
             }
             .navigationTitle("Library")
             .toolbar { ToolbarItem(placement: .navigationBarLeading) {
-                Button(action: dismiss, label: { Text("Done").fontWeight(.medium) })
+                Button(action: dismiss, label: { Text("Done").bold()})
             }}
             .background(Color(.systemGroupedBackground).edgesIgnoringSafeArea(.all))
         }.navigationViewStyle(StackNavigationViewStyle())
     }
 }
 
+struct ZimFilesQueryResult {
+    private(set) var categories = [ZimFile.Category]()
+    private(set) var metaData = [ZimFile.Category: [ZimFile]]()
+    
+    init(results: Results<ZimFile>? = nil) {
+        guard let results = results else { return }
+        for zimFile in results {
+            guard metaData[zimFile.category, default: []].count < 9 else { continue }
+            metaData[zimFile.category, default: []].append(zimFile)
+        }
+        categories = Array(metaData.keys).sorted()
+    }
+}
+
 @available(iOS 14.0, *)
 class LibraryViewModel: ObservableObject {
-    @Published var onDevice = [ZimFile]() {
-        didSet {
-            for zimFile in onDevice {
-                print(zimFile.categoryRaw)
-                groupIDs.insert(zimFile.groupID)
-            }
-            print(groupIDs)
-        }
-    }
-    var groupIDs = Set<String>()
-    private var onDeviceZimFilesPipeline: AnyCancellable? = nil
+    private let queue = DispatchQueue(label: "org.kiwix.libraryUI", qos: .userInitiated)
+    private let database: Realm?
+    private var zimFilesPipeline: AnyCancellable? = nil
+    @Published var result = ZimFilesQueryResult()
     
     init() {
-        do {
-            let database = try Realm(configuration: Realm.defaultConfig)
-            let predicate = NSPredicate(format: "languageCode == %@ AND NOT (categoryRaw IN %@)", "en", ["ted", "stack_exchange", "other"])
-            onDeviceZimFilesPipeline = database.objects(ZimFile.self)
-                .filter(predicate)
-                .sorted(byKeyPath: "size", ascending: false)
-                .collectionPublisher
-                .subscribe(on: DispatchQueue.main)
-                .freeze()
-                .map { Array($0) }
-                .receive(on: DispatchQueue.main)
-                .catch { _ in Just([]) }
-                .assign(to: \.onDevice, on: self)
-        } catch { }
+        self.database = try? Realm(configuration: Realm.defaultConfig)
+        
+        let predicate = NSPredicate(format: "languageCode == %@", "en")
+        zimFilesPipeline = database?.objects(ZimFile.self)
+            .filter(predicate)
+            .sorted(byKeyPath: "size", ascending: false)
+            .collectionPublisher
+            .subscribe(on: queue)
+            .freeze()
+            .map { ZimFilesQueryResult(results: $0) }
+            .receive(on: DispatchQueue.main)
+            .catch { _ in Just(ZimFilesQueryResult()) }
+            .assign(to: \.result, on: self)
     }
     
     deinit {
-        onDeviceZimFilesPipeline?.cancel()
+        zimFilesPipeline?.cancel()
     }
 }
