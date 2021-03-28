45d212392a301fd40d269249aa6abb56c7e00c6a
Chris Li <chris@kiwix.org>
2021-03-28 14:22:33 -0400

    cell structure

--- iOS/SwiftUI/ZimFileDetailView.swift
+++ iOS/SwiftUI/ZimFileDetailView.swift
@@ -14,50 +14,121 @@ import RealmSwift
 struct ZimFileDetailView: View {
     @StateRealmObject var zimFile: ZimFile
     
-    private let countFormatter = NumberFormatter()
-    
     init(fileID: String) {
         if let database = try? Realm(), let zimFile = database.object(ofType: ZimFile.self, forPrimaryKey: fileID) {
             self._zimFile = StateRealmObject(wrappedValue: zimFile)
         } else {
             self._zimFile = StateRealmObject(wrappedValue: ZimFile())
         }
-        countFormatter.numberStyle = .decimal
     }
     
     var body: some View {
         List {
             Section {
-                TitleDetailCell(title: "Language", detail: zimFile.localizedLanguageName)
-                TitleDetailCell(title: "Size", detail: zimFile.sizeDescription ?? "Unknown")
-                TitleDetailCell(title: "Date", detail: zimFile.creationDateDescription ?? "Unknown")
+                Text(zimFile.title)
+                Text(zimFile.fileDescription)
             }
             Section {
-                TitleDetailCell(title: "Picture", detail: zimFile.hasPictures ? "Yes" : "No")
-                TitleDetailCell(title: "Videos", detail: zimFile.hasVideos ? "Yes" : "No")
-                TitleDetailCell(title: "Details", detail: zimFile.hasDetails ? "Yes" : "No")
+                Cell(title: "Language", detail: zimFile.localizedLanguageName)
+                Cell(title: "Size", detail: zimFile.sizeDescription)
+                Cell(title: "Date", detail: zimFile.creationDateDescription)
             }
             Section {
-                TitleDetailCell(title: "Article Count", detail: formatCount(zimFile.articleCount.value))
-                TitleDetailCell(title: "Media Count", detail: formatCount(zimFile.mediaCount.value))
+                CheckmarkCell(title: "Picture", isChecked: zimFile.hasPictures)
+                CheckmarkCell(title: "Videos", isChecked: zimFile.hasVideos)
+                CheckmarkCell(title: "Details", isChecked: zimFile.hasDetails)
             }
             Section {
-                TitleDetailCell(title: "Creator", detail: zimFile.creator ?? "Unknown")
-                TitleDetailCell(title: "Publisher", detail: zimFile.publisher ?? "Unknown")
+                CountCell(title: "Article Count", count: zimFile.articleCount.value)
+                CountCell(title: "Media Count", count: zimFile.mediaCount.value)
             }
             Section {
-                TitleDetailCell(title: "ID", detail: String(zimFile.fileID.prefix(8)))
+                Cell(title: "Creator", detail: zimFile.creator)
+                Cell(title: "Publisher", detail: zimFile.publisher)
+            }
+            Section {
+                Cell(title: "ID", detail: String(zimFile.fileID.prefix(8)))
             }
         }
         .navigationTitle(zimFile.title)
         .listStyle(InsetGroupedListStyle())
     }
     
-    private func formatCount(_ value: Int64?) -> String {
-        if let value = value, let formatted = countFormatter.string(from: NSNumber(value: value)) {
-            return formatted
-        } else {
-            return "Unknown"
+    struct Cell: View {
+        let title: String
+        let detail: String?
+        
+        var body: some View {
+            HStack {
+                Text(title)
+                Spacer()
+                Text(detail ?? "Unknown").foregroundColor(.secondary)
+            }
+        }
+    }
+    
+    struct CountCell: View {
+        let title: String
+        let count: Int64?
+        private let formatter = NumberFormatter()
+        
+        init(title: String, count: Int64?) {
+            self.title = title
+            self.count = count
+            formatter.numberStyle = .decimal
+        }
+        
+        var body: some View {
+            HStack {
+                Text(title)
+                Spacer()
+                if let count = count, let formatted = NumberFormatter().string(from: NSNumber(value: count)) {
+                    Text(formatted).foregroundColor(.secondary)
+                } else {
+                    Text("Unknown").foregroundColor(.secondary)
+                }
+            }
+        }
+    }
+    
+    struct CheckmarkCell: View {
+        let title: String
+        let isChecked: Bool
+        
+        var body: some View {
+            HStack {
+                Text(title)
+                Spacer()
+                if isChecked{
+                    Image(systemName: "checkmark.circle.fill").foregroundColor(.green)
+                } else {
+                    Image(systemName: "multiply.circle.fill").foregroundColor(.secondary)
+                }
+            }
+        }
+    }
+    
+    struct ActionButton: View {
+        let title: String
+        let isDestructive: Bool
+        let action: (() -> Void)
+        
+        init(title: String, isDestructive: Bool = false, action: @escaping (() -> Void) = {}) {
+            self.title = title
+            self.isDestructive = isDestructive
+            self.action = action
+        }
+        
+        var body: some View {
+            Button(action: action, label: {
+                HStack {
+                    Spacer()
+                    Text(title)
+                        .fontWeight(.medium)
+                        .foregroundColor(isDestructive ? .red : nil)
+                    Spacer()
+                }
+            })
         }
     }
 }
