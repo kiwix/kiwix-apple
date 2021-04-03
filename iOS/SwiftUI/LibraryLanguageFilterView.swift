d50af839863b1b6fada96e58d2bf0860e67f0da2
ChrisLi <chris@kiwix.org>
2021-04-03 11:47:09 -0400

    show hiding

--- iOS/SwiftUI/LibraryLanguageFilterView.swift
+++ iOS/SwiftUI/LibraryLanguageFilterView.swift
@@ -7,17 +7,30 @@
 //
 
 import SwiftUI
+import RealmSwift
 
 @available(iOS 14.0, *)
 struct LibraryLanguageFilterView: View {
     @AppStorage("libraryLanguageSortingMode") var sortingMode: LibraryLanguageFilterSortingMode = .alphabetically
-    var doneButtonTapped: () -> Void = {}
     
+    @ObservedObject private var viewModel = ViewModel()
+    var doneButtonTapped: () -> Void = {}
     
     var body: some View {
         List {
-            
-        }.toolbar {
+            if viewModel.showing.count > 0 {
+                Section(header: Text("Showing")) {
+                    ForEach(viewModel.showing) { LanguageCell(language: $0) }
+                }
+            }
+            if viewModel.hiding.count > 0 {
+                Section(header: Text("Hiding")) {
+                    ForEach(viewModel.hiding) { LanguageCell(language: $0) }
+                }
+            }
+        }
+        .listStyle(GroupedListStyle())
+        .toolbar {
             ToolbarItem(placement: .navigationBarLeading) {
                 Button("Done", action: doneButtonTapped)
             }
@@ -30,13 +43,93 @@ struct LibraryLanguageFilterView: View {
             }
         }
     }
-}
-
-@available(iOS 14.0, *)
-struct LibraryLanguageFilterView_Previews: PreviewProvider {
-    static var previews: some View {
-        LibraryLanguageFilterView()
-            .previewDevice(PreviewDevice(rawValue: "iPhone 12 Pro"))
-            .previewDisplayName("iPhone 12 Pro")
+    
+    struct Language: Identifiable, Comparable {
+        var id: String { code }
+        let code: String
+        let name: String
+        let count: Int
+        
+        init?(code: String, count: Int) {
+            guard let name = Locale.current.localizedString(forLanguageCode: code) else { return nil }
+            self.code = code
+            self.name = name
+            self.count = count
+        }
+        
+        static func < (lhs: LibraryLanguageFilterView.Language, rhs: LibraryLanguageFilterView.Language) -> Bool {
+            switch lhs.name.caseInsensitiveCompare(rhs.name) {
+            case .orderedAscending:
+                return true
+            case .orderedDescending:
+                return false
+            case .orderedSame:
+                return lhs.count > rhs.count
+            }
+        }
+    }
+    
+    @available(iOS 14.0, *)
+    struct LanguageCell: View {
+        let language: Language
+        var body: some View {
+            HStack {
+                Text(language.name)
+                Spacer()
+                Text("\(language.count)").foregroundColor(.secondary)
+            }
+        }
+    }
+    
+    class ViewModel: ObservableObject {
+        @AppStorage("libraryLanguageSortingMode") var sortingMode: LibraryLanguageFilterSortingMode = .alphabetically
+        @Published private(set) var showing: [Language] = []
+        @Published private(set) var hiding: [Language] = []
+        
+        init() {
+            DispatchQueue.global(qos: .userInitiated).async {
+                do {
+                    var showing: [Language] = []
+                    var hiding: [Language] = []
+                    let showinglanguageCodes = UserDefaults.standard.libraryFilterLanguageCodes
+                    
+                    let database = try Realm()
+                    let codes = database.objects(ZimFile.self).distinct(by: ["languageCode"]).map({ $0.languageCode })
+                    for code in codes {
+                        let count = database.objects(ZimFile.self).filter("languageCode = %@", code).count
+                        guard let language = Language(code: code, count: count) else { continue }
+                        if showinglanguageCodes.contains(code) {
+                            showing.append(language)
+                        } else {
+                            hiding.append(language)
+                        }
+                    }
+                    
+                    self.sort(&showing)
+                    self.sort(&hiding)
+                    
+                    DispatchQueue.main.async {
+                        self.showing = showing
+                        self.hiding = hiding
+                    }
+                } catch {}
+            }
+        }
+        
+        private func sort(_ languages: inout [Language]) {
+            switch sortingMode {
+            case .alphabetically:
+                languages.sort { $0 < $1 }
+            case .byCount:
+                languages.sort {
+                    if $0.count == $1.count {
+                        return $0 < $1
+                    } else {
+                        return $0.count > $1.count
+                    }
+                }
+            }
+        }
     }
 }
+
