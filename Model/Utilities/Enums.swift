a7ec306138a7f5850a0d4985d2644ce41b78a887
ChrisLi <chris@kiwix.org>
2021-04-04 12:14:35 -0400

    enum value

--- Model/Utilities/Enums.swift
+++ Model/Utilities/Enums.swift
@@ -22,6 +22,10 @@ enum ExternalLinkLoadingPolicy: Int, CaseIterable, Codable, CustomStringConverti
     }
 }
 
+enum LibraryCategorySortingMode: String {
+    case alphabetical, size, date
+}
+
 enum LibraryLanguageFilterSortingMode: String, Codable, CustomStringConvertible {
     case alphabetically, byCount
     
