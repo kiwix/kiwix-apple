316a2ff2a25e09de3e43e7469c78bc4c738ed49c
ChrisLi <chris@kiwix.org>
2021-04-19 08:57:47 -0400

    LibraryInfoView

--- iOS/SwiftUI/Library/LibraryInfoView.swift
+++ iOS/SwiftUI/Library/LibraryInfoView.swift
@@ -10,7 +10,30 @@ import SwiftUI
 
 @available(iOS 14.0, *)
 struct LibraryInfoView: View {
+    @AppStorage("libraryAutoRefresh") private var autoRefresh: Bool = true
+    @AppStorage("backupDocumentDirectory") private var backupDocumentDirectory: Bool = false
+    
     var body: some View {
-        Text("Hello, World!")
+        List {
+            Section(header: Text("Catalog")) {
+                ActionCell(title: "Update Now") {
+                    
+                }
+            }
+            Section {
+                HStack {
+                    Text("Last update")
+                }
+                Toggle(isOn: $autoRefresh, label: { Text("Auto update") })
+            }
+            Section(header: Text("Backup"), footer: Text("Does not apply to files that were opened in place.")) {
+                Toggle(
+                    isOn: $backupDocumentDirectory,
+                    label: { Text("Include files in backup") }
+                ).onChange(of: backupDocumentDirectory, perform: { value in
+                    
+                })
+            }
+        }.listStyle(InsetGroupedListStyle())
     }
 }
