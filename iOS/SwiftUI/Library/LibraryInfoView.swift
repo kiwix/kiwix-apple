92225c631565a1b4faa6326120a4de50abec40d2
ChrisLi <chris@kiwix.org>
2021-04-20 18:20:13 -0400

    last update section

--- iOS/SwiftUI/Library/LibraryInfoView.swift
+++ iOS/SwiftUI/Library/LibraryInfoView.swift
@@ -20,9 +20,24 @@ struct LibraryInfoView: View {
                     
                 }
             }
-            Section {
+            Section(footer: Text(
+                """
+                When enabled, the library catalog will be updated both when library is opened \
+                and utilizing iOS's Background App Refresh feature.
+                """
+            )) {
                 HStack {
                     Text("Last update")
+                    Spacer()
+                    if let lastRefreshTime = UserDefaults.standard.value(forKey: "libraryLastRefreshTime") as? Date {
+                        if Date().timeIntervalSince(lastRefreshTime) < 120 {
+                            Text("Just Now")
+                        } else {
+                            Text(RelativeDateTimeFormatter().localizedString(for: lastRefreshTime, relativeTo: Date()))
+                        }
+                    } else {
+                        Text("Never")
+                    }
                 }
                 Toggle(
                     isOn: $autoRefresh, label: { Text("Auto update") }
