30ad628916982360693f5e09b03c425abd38bf82
ChrisLi <chris@kiwix.org>
2021-06-12 16:55:21 -0400

    data model

---
+++ Model/Persistence/PersistentContainer.swift
@@ -0,0 +1,48 @@
+//
+//  PersistentContainer.swift
+//  Kiwix
+//
+//  Created by Chris Li on 6/12/21.
+//  Copyright © 2021 Chris Li. All rights reserved.
+//
+
+import CoreData
+
+struct PersistenceController {
+    static let shared = PersistenceController()
+    let container: NSPersistentContainer
+
+    init(inMemory: Bool = false) {
+        container = NSPersistentContainer(name: "model")
+
+        if inMemory {
+            container.persistentStoreDescriptions.first?.url = URL(fileURLWithPath: "/dev/null")
+        }
+        
+        container.loadPersistentStores { description, error in
+            if let error = error {
+                fatalError("Error: \(error.localizedDescription)")
+            }
+        }
+    }
+    
+    func save() {
+        if container.viewContext.hasChanges {
+            do {
+                try container.viewContext.save()
+            } catch {
+                // Show some error here
+            }
+        }
+    }
+}
+
+class Persistent: NSPersistentContainer {
+    static let shared = Persistent()
+    
+    convenience init() {
+        self.init(name: "model")
+        loadPersistentStores { description, error in
+        }
+    }
+}
