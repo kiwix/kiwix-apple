a68718b574a972f32b4351a1888fa4f1a239257f
ChrisLi <chris@kiwix.org>
2021-09-13 22:41:19 -0400

    split out SettingsViewController

--- iOS/SwiftUI/SettingsView.swift
+++ iOS/SwiftUI/SettingsView.swift
@@ -6,56 +6,10 @@
 //  Copyright © 2020 Chris Li. All rights reserved.
 //
 
-import MessageUI
 import SafariServices
 import SwiftUI
 
 import Defaults
-
-@available(iOS 13.0, *)
-class SettingsViewController: UIHostingController<SettingsView>, MFMailComposeViewControllerDelegate {
-    convenience init() {
-        self.init(rootView: SettingsView())
-        rootView.dismiss = { [unowned self] in self.dismiss(animated: true) }
-        rootView.sendFeedback = { [unowned self] in self.presentFeedbackEmailComposer() }
-    }
-    
-    private func presentFeedbackEmailComposer() {
-        let version = Bundle.main.infoDictionary?["CFBundleShortVersionString"] as! String
-        let controller = MFMailComposeViewController()
-        controller.setToRecipients(["feedback@kiwix.org"])
-        controller.setSubject("Feedback of Kiwix for iOS v\(version)")
-        controller.mailComposeDelegate = self
-        present(controller, animated: true)
-    }
-    
-    func mailComposeController(_ controller: MFMailComposeViewController,
-                               didFinishWith result: MFMailComposeResult,
-                               error: Error?) {
-        controller.dismiss(animated: true)
-        switch result {
-        case .sent:
-            let alert = UIAlertController(
-                title: NSLocalizedString("Email Sent", comment: "Feedback Email"),
-                message: NSLocalizedString("We will read your message as soon as possible.", comment: "Feedback Email"),
-                preferredStyle: .alert
-            )
-            alert.addAction(UIAlertAction(title: NSLocalizedString("OK", comment: "Feedback Email"), style: .default))
-            present(alert, animated: true)
-        case .failed:
-            guard let error = error else {break}
-            let alert = UIAlertController(
-                title: NSLocalizedString("Email Not Sent", comment: "Feedback Email"),
-                message: error.localizedDescription,
-                preferredStyle: .alert
-            )
-            alert.addAction(UIAlertAction(title: NSLocalizedString("OK", comment: "Feedback Email"), style: .default))
-            present(alert, animated: true)
-        default:
-            break
-        }
-    }
-}
 
 @available(iOS 13.0, *)
 struct SettingsView: View {
