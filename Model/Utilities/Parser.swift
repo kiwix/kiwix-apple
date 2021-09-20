ba9232fe715e7997e4e232fbf66f7e385d2a38b0
ChrisLi <chris@kiwix.org>
2021-09-20 08:58:05 -0400

    parser

--- Model/Utilities/Parser.swift
+++ Model/Utilities/Parser.swift
@@ -11,6 +11,7 @@ import CoreLocation
     import NaturalLanguage
 #endif
 import SwiftSoup
+import Fuzi
 
 class Parser {
     private let document: Document
@@ -31,14 +32,7 @@ class Parser {
             let html = String(data: data, encoding: .utf8) else { throw NSError() }
         try self.init(html: html)
     }
-    
-    func getTitle() -> String? {
-        do {
-            let elements = try document.select("head > title")
-            return try elements.first()?.text()
-        } catch { return nil }
-    }
-    
+
     func getGeoCoordinate() -> CLLocationCoordinate2D? {
         do {
             let elements = try document.select("head > meta[name='geo.position']")
@@ -46,49 +40,6 @@ class Parser {
             guard let parts = content?.split(separator: ";"), parts.count == 2,
                 let lat = Double(parts[0]), let lon = Double(parts[1]) else { return nil }
             return CLLocationCoordinate2D(latitude: lat, longitude: lon)
-        } catch { return nil }
-    }
-
-    func getFirstParagraph() -> NSAttributedString? {
-        let snippet = NSMutableAttributedString()
-        for node in firstParagraph?.getChildNodes() ?? [] {
-            if let element = node as? Element {
-                if let className = try? element.className(), className == "mw-ref" {
-                    continue
-                } else if let text = try? element.text() {
-                    let attributedSting = NSAttributedString(
-                        string: text,
-                        attributes: element.tagName() == "b" ? [.font: Parser.boldFont] : nil
-                    )
-                    snippet.append(attributedSting)
-                }
-            } else if let text = try? node.outerHtml() {
-                snippet.append(NSAttributedString(string: text))
-            }
-        }
-        return snippet.string.trimmingCharacters(in: .whitespacesAndNewlines).isEmpty ? nil : snippet
-    }
-    
-    @available(iOS 12.0, macOS 10.14, *)
-    func getFirstSentence(languageCode: String?) -> NSAttributedString? {
-        guard let firstParagraph = self.getFirstParagraph() else { return nil }
-        let text = firstParagraph.string
-        var firstSentence: NSAttributedString?
-        
-        let tokenizer = NLTokenizer(unit: .sentence)
-        if let languageCode = languageCode {tokenizer.setLanguage(NLLanguage(languageCode))}
-        tokenizer.string = text
-        tokenizer.enumerateTokens(in: text.startIndex..<text.endIndex) { range, _ in
-            firstSentence = firstParagraph.attributedSubstring(from: NSRange(range, in: firstParagraph.string))
-            return false
-        }
-        return firstSentence
-    }
-    
-    func getFirstImagePath() -> String? {
-        do {
-            let element = try document.getElementsByTag("img").first()
-            return element?.getAttributes()?.get(key: "src")
         } catch { return nil }
     }
     
@@ -116,5 +67,72 @@ class Parser {
             }
         }
         return snippet
+    }
+}
+
+class Parser2 {
+    private let document: HTMLDocument
+    
+    static private let boldFont = NSUIFont.systemFont(ofSize: 12.0, weight: .medium)
+    
+    init(document: HTMLDocument) {
+        self.document = document
+    }
+    
+    convenience init(html: Data) throws {
+        self.init(document: try HTMLDocument(data: html))
+    }
+    
+    convenience init(zimFileID: String, path: String) throws {
+        guard let data = ZimFileService.shared.getData(zimFileID: zimFileID, contentPath: path) else { throw NSError() }
+        try self.init(html: data)
+    }
+    
+    func getTitle() -> String? {
+        document.title
+    }
+    
+    func getFirstParagraph() -> NSAttributedString? {
+        guard let firstParagraph = document.firstChild(xpath: "//p") else { return nil }
+        let snippet = NSMutableAttributedString()
+        for child in firstParagraph.childNodes(ofTypes: [.Text, .Element]) {
+            if let element = child as? XMLElement {
+                let attributedSting = NSAttributedString(
+                    string: element.stringValue,
+                    attributes: element.tag == "b" ? [.font: Parser2.boldFont] : nil
+                )
+                snippet.append(attributedSting)
+            } else {
+                snippet.append(NSAttributedString(string: child.stringValue))
+            }
+        }
+        return snippet.string.trimmingCharacters(in: .whitespacesAndNewlines).isEmpty ? nil : snippet
+    }
+    
+    func getFirstSentence(languageCode: String?) -> NSAttributedString? {
+        guard let firstParagraph = self.getFirstParagraph() else { return nil }
+        let text = firstParagraph.string
+        var firstSentence: NSAttributedString?
+        
+        let tokenizer = NLTokenizer(unit: .sentence)
+        if let languageCode = languageCode {tokenizer.setLanguage(NLLanguage(languageCode))}
+        tokenizer.string = text
+        tokenizer.enumerateTokens(in: text.startIndex..<text.endIndex) { range, _ in
+            firstSentence = firstParagraph.attributedSubstring(from: NSRange(range, in: firstParagraph.string))
+            return false
+        }
+        return firstSentence
+    }
+    
+    func getFirstImagePath() -> String? {
+        guard let firstImage = document.firstChild(xpath: "//img") else { return nil }
+        return firstImage.attributes["src"]
+    }
+
+    class func test() {
+        let url = URL(string: "kiwix://aca10302-c60d-f47e-1733-4a6ae9d88c07/A/Global_catastrophic_risk")!
+        let content = ZimFileService.shared.getURLContent(url: url)!.data
+        let t = (try! Parser2(html: content)).getFirstParagraph()
+        print(t)
     }
 }
