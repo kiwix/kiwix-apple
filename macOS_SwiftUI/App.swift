049d2b602f9a203108733412ab40c50f1327dc0d
ChrisLi <chris@kiwix.org>
2021-10-15 21:17:49 -0400

    commands

--- macOS_SwiftUI/App.swift
+++ macOS_SwiftUI/App.swift
@@ -48,11 +48,38 @@ struct Kiwix: App {
             }
             .navigationTitle("Article Name")
             .navigationSubtitle("example.zim")
+        }.commands {
+            SidebarCommands()
+            CommandGroup(after: CommandGroupPlacement.newItem) {
+                Button("New Tab") { newTab() }.keyboardShortcut("t")
+                Divider()
+                Button("Open...") { open() }.keyboardShortcut("o")
+            }
         }
     }
     
     private func toggleSidebar() {
         NSApp.keyWindow?.firstResponder?.tryToPerform(#selector(NSSplitViewController.toggleSidebar(_:)), with: nil)
+    }
+    
+    private func newTab() {
+        guard let currentWindow = NSApp.keyWindow, let windowController = currentWindow.windowController else { return }
+        windowController.newWindowForTab(nil)
+        guard let newWindow = NSApp.keyWindow, currentWindow != newWindow else { return }
+        currentWindow.addTabbedWindow(newWindow, ordered: .above)
+    }
+    
+    private func open() {
+        guard let window = NSApp.keyWindow else { return }
+        let panel = NSOpenPanel()
+        panel.showsHiddenFiles = false
+        panel.canChooseFiles = true
+        panel.canChooseDirectories = false
+        panel.allowsMultipleSelection = false
+        panel.allowedFileTypes = ["zim"]
+        panel.beginSheetModal(for: window) { response in
+            
+        }
     }
 }
 
