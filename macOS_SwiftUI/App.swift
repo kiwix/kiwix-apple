b41fa347961f4ee9a00e070eb2ad7325f5b8cac2
ChrisLi <chris@kiwix.org>
2021-10-19 08:38:54 -0400

    initial commit

---
+++ macOS_SwiftUI/App.swift
@@ -0,0 +1,18 @@
+//
+//  KiwixApp.swift
+//  Kiwix
+//
+//  Created by Chris Li on 10/19/21.
+//  Copyright © 2021 Chris Li. All rights reserved.
+//
+
+import SwiftUI
+
+@main
+struct KiwixApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ macOS_SwiftUI/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  Kiwix
+//
+//  Created by Chris Li on 10/19/21.
+//  Copyright © 2021 Chris Li. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
