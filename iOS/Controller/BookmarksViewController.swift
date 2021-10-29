651f563e3d53f30694584ffbb376a595f4c060f8
ChrisLi <chris@kiwix.org>
2021-10-28 22:50:35 -0400

    empty content view

--- iOS/Controller/BookmarksViewController.swift
+++ iOS/Controller/BookmarksViewController.swift
@@ -9,6 +9,8 @@
 import SwiftUI
 import UIKit
 
+import RealmSwift
+
 
 class BookmarksViewController: UIHostingController<BookmarksView> {
     convenience init() {
@@ -31,7 +33,37 @@ class BookmarksViewController: UIHostingController<BookmarksView> {
 }
 
 struct BookmarksView: View {
+    @ObservedResults(
+        Bookmark.self,
+        sortDescriptor: SortDescriptor(keyPath: "date", ascending: false)
+    ) private var bookmarks
+    
     var body: some View {
-        Text("Hello!")
+        if bookmarks.isEmpty {
+            VStack(spacing: 10) {
+                ZStack {
+                    Image(systemName: "star.fill")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .padding(20)
+                        .foregroundColor(.secondary)
+                    Circle().foregroundColor(.secondary).opacity(0.2)
+                }.frame(width: 75, height: 75, alignment: .center)
+                VStack(spacing: 6) {
+                    Text("No bookmarked article.").font(Font.headline)
+                    Text("To add, long press the bookmark button on the tool bar when reading an article.")
+                        .multilineTextAlignment(.center)
+                        .font(.footnote)
+                }.padding()
+            }.navigationBarTitle("Bookmarks")
+        } else {
+            List {
+                ForEach(bookmarks) { bookmark in
+                    Text(bookmark.title)
+                }
+            }
+            .listStyle(.plain)
+            .navigationBarTitle("Bookmarks")
+        }
     }
 }
