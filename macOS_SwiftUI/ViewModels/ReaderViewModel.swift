12ecf17d595bb2376ea0f4e3b450f6765eda814d
Chris Li <chris@kiwix.org>
2022-02-13 15:19:18 -0500

    ReaderViewModel

---
+++ macOS_SwiftUI/ViewModels/ReaderViewModel.swift
@@ -0,0 +1,172 @@
+//
+//  ReaderViewModel.swift
+//  Kiwix for macOS
+//
+//  Created by Chris Li on 2/13/22.
+//  Copyright © 2022 Chris Li. All rights reserved.
+//
+
+import WebKit
+
+class ReaderViewModel: NSObject, ObservableObject, WKNavigationDelegate, WKScriptMessageHandler {
+    @Published private(set) var canGoBack: Bool = false
+    @Published private(set) var canGoForward: Bool = false
+    @Published private(set) var articleTitle: String = ""
+    @Published private(set) var zimFileName: String = ""
+    @Published private(set) var outlineItems = [OutlineItem]()
+    
+    let webView: WKWebView = {
+        let config = WKWebViewConfiguration()
+        config.setURLSchemeHandler(KiwixURLSchemeHandler(), forURLScheme: "kiwix")
+        config.userContentController = {
+            let controller = WKUserContentController()
+            guard FeatureFlags.wikipediaDarkUserCSS,
+                  let path = Bundle.main.path(forResource: "wikipedia_dark", ofType: "css"),
+                  let css = try? String(contentsOfFile: path) else { return controller }
+            let source = """
+                var style = document.createElement('style');
+                style.innerHTML = `\(css)`;
+                document.head.appendChild(style);
+                """
+            let script = WKUserScript(source: source, injectionTime: .atDocumentEnd, forMainFrameOnly: false)
+            controller.addUserScript(script)
+            if let url = Bundle.main.url(forResource: "injection", withExtension: "js"),
+               let javascript = try? String(contentsOf: url) {
+                let script = WKUserScript(source: javascript, injectionTime: .atDocumentEnd, forMainFrameOnly: false)
+                controller.addUserScript(script)
+            }
+            return controller
+        }()
+        return WKWebView(frame: .zero, configuration: config)
+    }()
+    private var canGoBackObserver: NSKeyValueObservation?
+    private var canGoForwardObserver: NSKeyValueObservation?
+    private var titleObserver: NSKeyValueObservation?
+    
+    override init() {
+        super.init()
+        webView.navigationDelegate = self
+        canGoBackObserver = webView.observe(\.canGoBack) { [unowned self] webView, _ in
+            self.canGoBack = webView.canGoBack
+        }
+        canGoForwardObserver = webView.observe(\.canGoForward) { [unowned self] webView, _ in
+            self.canGoForward = webView.canGoForward
+        }
+        titleObserver = webView.observe(\.title) { [unowned self] webView, _ in
+            guard let title = webView.title, !title.isEmpty,
+                  let zimFileID = webView.url?.host,
+                  let zimFile = try? Database.shared.container.viewContext.fetch(
+                    ZimFile.fetchRequest(predicate: NSPredicate(format: "fileID == %@", zimFileID))
+                  ).first else { return }
+            self.articleTitle = title
+            self.zimFileName = zimFile.name
+        }
+        
+        webView.configuration.userContentController.add(self, name: "headings")
+        webView.configuration.userContentController.add(self, name: "headingVisible")
+    }
+    
+    func loadMainPage(zimFileID: UUID? = nil) {
+        let zimFileID = zimFileID?.uuidString ?? webView.url?.host ?? ""
+        guard let url = ZimFileService.shared.getMainPageURL(zimFileID: zimFileID) else { return }
+        webView.load(URLRequest(url: url))
+    }
+    
+    func loadRandomPage(zimFileID: UUID? = nil) {
+        let zimFileID = zimFileID?.uuidString ?? webView.url?.host ?? ""
+        guard let url = ZimFileService.shared.getRandomPageURL(zimFileID: zimFileID) else { return }
+        webView.load(URLRequest(url: url))
+    }
+    
+    // MARK: - WKNavigationDelegate
+    
+    func webView(_ webView: WKWebView,
+                 decidePolicyFor navigationAction: WKNavigationAction) async -> WKNavigationActionPolicy {
+        guard let url = navigationAction.request.url else { return .cancel }
+        if url.isKiwixURL, let redirectedURL = ZimFileService.shared.getRedirectedURL(url: url) {
+            DispatchQueue.main.async { webView.load(URLRequest(url: redirectedURL)) }
+            return .cancel
+        } else if url.isKiwixURL {
+            return .allow
+        } else if url.scheme == "http" || url.scheme == "https" {
+            DispatchQueue.main.async { NSWorkspace.shared.open(url) }
+            return .cancel
+        } else if url.scheme == "geo" {
+            let coordinate = url.absoluteString.replacingOccurrences(of: "geo:", with: "")
+            if let url = URL(string: "http://maps.apple.com/?ll=\(coordinate)") {
+                DispatchQueue.main.async { NSWorkspace.shared.open(url) }
+            }
+            return .cancel
+        } else {
+            return .cancel
+        }
+    }
+    
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        webView.evaluateJavaScript("expandAllDetailTags(); getOutlineItems();")
+    }
+    
+    // MARK: - WKScriptMessageHandler
+    
+    func userContentController(_ userContentController: WKUserContentController, didReceive message: WKScriptMessage) {
+        if message.name == "headings", let headings = message.body as? [[String: String]] {
+            DispatchQueue.global(qos: .userInitiated).async {
+                self.generateOutlineTree(headings: headings)
+            }
+        }
+    }
+    
+    // MARK: - Outlines
+    
+    /// Scroll to a outline item
+    /// - Parameter outlineItemID: ID of the outline item to scroll to
+    func scrollTo(outlineItemID: String) {
+        webView.evaluateJavaScript("scrollToHeading('\(outlineItemID)')")
+    }
+    
+    /// Convert flattened heading element data to a tree of OutlineItems.
+    /// - Parameter headings: list of heading element data retrieved from webview
+    private func generateOutlineTree(headings: [[String: String]]) {
+        let root = OutlineItem(index: -1, text: "", level: 0)
+        var stack: [OutlineItem] = [root]
+        var all = [String: OutlineItem]()
+        
+        headings.enumerated().forEach { index, heading in
+            guard let id = heading["id"],
+                  let text = heading["text"],
+                  let tag = heading["tag"], let level = Int(tag.suffix(1)) else { return }
+            let item = OutlineItem(id: id, index: index, text: text, level: level)
+            all[item.id] = item
+            
+            // get last item in stack
+            // if last item is child of item's sibling, unwind stack until a sibling is found
+            guard var lastItem = stack.last else { return }
+            while lastItem.level > item.level {
+                stack.removeLast()
+                lastItem = stack[stack.count - 1]
+            }
+            
+            // if item is last item's sibling, add item to parent and replace last item with itself in stack
+            // if item is last item's child, add item to parent and add item to stack
+            if lastItem.level == item.level {
+                stack[stack.count - 2].addChild(item)
+                stack[stack.count - 1] = item
+            } else if lastItem.level < item.level {
+                stack[stack.count - 1].addChild(item)
+                stack.append(item)
+            }
+        }
+        
+        // if there is only one h1, flatten one level
+        if let rootChildren = root.children, rootChildren.count == 1, let rootFirstChild = rootChildren.first {
+            let children = rootFirstChild.removeAllChildren()
+            DispatchQueue.main.async {
+                self.outlineItems = [rootFirstChild] + children
+            }
+        } else {
+            DispatchQueue.main.async {
+                self.outlineItems = root.children ?? []
+            }
+        }
+    }
+}
