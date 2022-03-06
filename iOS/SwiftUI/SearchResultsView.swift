aaeae3caadd035a531a0b4e59afbffb2ab8eca69
Chris Li <chris@kiwix.org>
2022-03-06 09:21:50 -0500

    favicon data

--- iOS/SwiftUI/SearchResultsView.swift
+++ iOS/SwiftUI/SearchResultsView.swift
@@ -321,7 +321,7 @@ private struct ResultsListView: View {
                     viewModel.updateRecentSearchTexts()
                 } label: {
                     HStack(alignment: result.snippet == nil ? .center : .top) {
-                        Favicon(data: viewModel.onDeviceZimFiles[result.zimFileID.uuidString]?.faviconData)
+                        Favicon(data: viewModel.onDeviceZimFiles[result.zimFileID.uuidString.lowercased()]?.faviconData)
                         VStack(alignment: .leading) {
                             Text(result.title).fontWeight(.medium).lineLimit(1)
                             if #available(iOS 15.0, *), let snippet = result.snippet {
