9e862588b64cfe232c9c6c7e6d88dd07833028c4
Chris Li <chris@kiwix.org>
2022-04-23 17:28:59 -0400

    move files

---
+++ iOS_SwiftUI/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  iOS_SwiftUI
+//
+//  Created by Chris Li on 4/23/22.
+//  Copyright © 2022 Chris Li. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
