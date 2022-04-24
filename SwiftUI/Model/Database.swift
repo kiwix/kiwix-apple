c67a5e35d222631dccdc3e902b3af6f0975c6a4f
Chris Li <chris@kiwix.org>
2022-04-24 09:08:24 -0400

    withCheckedThrowingContinuation

--- SwiftUI/Model/Database.swift
+++ SwiftUI/Model/Database.swift
@@ -17,7 +17,7 @@ class Database {
     private init() {
         notificationToken = NotificationCenter.default.addObserver(
             forName: .NSPersistentStoreRemoteChange, object: nil, queue: nil) { notification in
-                Task { try? await self.mergeChanges() }
+                try? self.mergeChanges()
         }
         token = {
             guard let data = UserDefaults.standard.data(forKey: "PersistentHistoryToken") else { return nil }
@@ -65,22 +65,35 @@ class Database {
         let context = container.newBackgroundContext()
         context.mergePolicy = NSMergePolicy.mergeByPropertyObjectTrump
         context.undoManager = nil
-        try await context.perform {
-            let predicate = NSPredicate(format: "fileID == %@", metadata.fileID as CVarArg)
-            let fetchRequest = ZimFile.fetchRequest(predicate: predicate)
-            let zimFile = try context.fetch(fetchRequest).first ?? ZimFile(context: context)
-            self.configureZimFile(zimFile, metadata: metadata)
-            zimFile.fileURLBookmark = fileURLBookmark
-            if context.hasChanges { try context.save() }
+        try await withCheckedThrowingContinuation { (continuation: CheckedContinuation<Void, Error>) -> Void in
+            context.performAndWait {
+                do {
+                    let predicate = NSPredicate(format: "fileID == %@", metadata.fileID as CVarArg)
+                    let fetchRequest = ZimFile.fetchRequest(predicate: predicate)
+                    let zimFile = try context.fetch(fetchRequest).first ?? ZimFile(context: context)
+                    self.configureZimFile(zimFile, metadata: metadata)
+                    zimFile.fileURLBookmark = fileURLBookmark
+                    if context.hasChanges { try context.save() }
+                } catch {
+                    continuation.resume(throwing: error)
+                }
+            }
+            continuation.resume()
         }
     }
     
     /// Batch update the local zim file database with what's available online.
     func refreshZimFileCatalog() async throws {
         guard let url = URL(string: "https://library.kiwix.org/catalog/root.xml") else { return }
-        let (data, response) = try await URLSession.shared.data(from: url)
-        guard let response = response as? HTTPURLResponse, response.statusCode == 200 else {
-            throw OPDSRefreshError.retrieve(description: "Error retrieving online catalog.")
+        let data: Data = try await withCheckedThrowingContinuation { continuation in
+            URLSession.shared.dataTask(with: url) { data, response, error in
+                guard let response = response as? HTTPURLResponse, response.statusCode == 200, let data = data else {
+                    let error = OPDSRefreshError.retrieve(description: "Error retrieving online catalog.")
+                    continuation.resume(throwing: error)
+                    return
+                }
+                continuation.resume(returning: data)
+            }.resume()
         }
         
         let parser = OPDSStreamParser()
@@ -92,63 +105,88 @@ class Database {
         context.undoManager = nil
         
         // insert new zim files
-        do {
-            var zimFileIDs = Set(parser.zimFileIDs)
-            try await context.perform {
-                zimFileIDs.subtract(try context.fetch(ZimFile.fetchRequest()).map { $0.fileID.uuidString.lowercased() })
-                let insertRequest = NSBatchInsertRequest(entity: ZimFile.entity(), managedObjectHandler: { zimFile in
-                    guard let zimFile = zimFile as? ZimFile else { return true }
-                    while !zimFileIDs.isEmpty {
-                        guard let id = zimFileIDs.popFirst(),
-                              let metadata = parser.getZimFileMetaData(id: id) else { continue }
-                        self.configureZimFile(zimFile, metadata: metadata)
-                        return false
-                    }
-                    return true
-                })
-                insertRequest.resultType = .count
-                guard let result = try context.execute(insertRequest) as? NSBatchInsertResult,
-                      let count = result.result as? Int else { throw OPDSRefreshError.process }
-                print("Added \(count) zim files entities.")
+        try await withCheckedThrowingContinuation { (continuation: CheckedContinuation<Void, Error>) -> Void in
+            context.performAndWait {
+                do {
+                    let existing = try context.fetch(ZimFile.fetchRequest()).map { $0.fileID.uuidString.lowercased() }
+                    var zimFileIDs = Set(parser.zimFileIDs).subtracting(existing)
+                    let insertRequest = NSBatchInsertRequest(
+                        entity: ZimFile.entity(),
+                        managedObjectHandler: { zimFile in
+                            guard let zimFile = zimFile as? ZimFile else { return true }
+                            while !zimFileIDs.isEmpty {
+                                guard let id = zimFileIDs.popFirst(),
+                                      let metadata = parser.getZimFileMetaData(id: id) else { continue }
+                                self.configureZimFile(zimFile, metadata: metadata)
+                                return false
+                            }
+                            return true
+                        }
+                    )
+                    insertRequest.resultType = .count
+                    guard let result = try context.execute(insertRequest) as? NSBatchInsertResult,
+                          let count = result.result as? Int else { throw OPDSRefreshError.process }
+                    print("Added \(count) zim files entities.")
+                } catch {
+                    continuation.resume(throwing: OPDSRefreshError.process)
+                }
             }
-        } catch {
-            throw OPDSRefreshError.process
+            continuation.resume()
         }
         
         // delete old zim files
-        do {
-            let fetchRequest: NSFetchRequest<NSFetchRequestResult> = ZimFile.fetchRequest()
-            fetchRequest.predicate = NSCompoundPredicate(andPredicateWithSubpredicates: [
-                NSPredicate(format: "fileURLBookmark == nil"),
-                NSPredicate(format: "NOT fileID IN %@", parser.zimFileIDs.compactMap { UUID(uuidString: $0) })
-            ])
-            let deleteRequest = NSBatchDeleteRequest(fetchRequest: fetchRequest)
-            deleteRequest.resultType = .resultTypeCount
-            try await context.perform {
-                guard let result = try context.execute(deleteRequest) as? NSBatchDeleteResult,
-                      let count = result.result as? Int else { throw OPDSRefreshError.process }
-                print("Deleted \(count) zim files entities.")
+        try await withCheckedThrowingContinuation { (continuation: CheckedContinuation<Void, Error>) -> Void in
+            context.performAndWait {
+                let fetchRequest: NSFetchRequest<NSFetchRequestResult> = ZimFile.fetchRequest()
+                fetchRequest.predicate = NSCompoundPredicate(andPredicateWithSubpredicates: [
+                    NSPredicate(format: "fileURLBookmark == nil"),
+                    NSPredicate(format: "NOT fileID IN %@", parser.zimFileIDs.compactMap { UUID(uuidString: $0) })
+                ])
+                let deleteRequest = NSBatchDeleteRequest(fetchRequest: fetchRequest)
+                deleteRequest.resultType = .resultTypeCount
+                do {
+                    guard let result = try context.execute(deleteRequest) as? NSBatchDeleteResult,
+                          let count = result.result as? Int else { throw OPDSRefreshError.process }
+                    print("Deleted \(count) zim files entities.")
+                } catch {
+                    continuation.resume(throwing: OPDSRefreshError.process)
+                }
             }
-        } catch {
-            throw OPDSRefreshError.process
+            continuation.resume()
         }
     }
     
     /// Save image data to zim files.
     func saveImageData(url: URL) async throws {
-        let (data, response) = try await URLSession.shared.data(from: url)
-        guard let response = response as? HTTPURLResponse,
-              let mimeType = response.mimeType,
-              response.statusCode == 200,
-              mimeType.contains("image") else { return }
+        let data: Data = try await withCheckedThrowingContinuation { continuation in
+            URLSession.shared.dataTask(with: url) { data, response, error in
+                guard let response = response as? HTTPURLResponse,
+                      response.statusCode == 200,
+                      let mimeType = response.mimeType,
+                      mimeType.contains("image"),
+                      let data = data else {
+                    let error = OPDSRefreshError.retrieve(description: "Error retrieving online catalog.")
+                    continuation.resume(throwing: error)
+                    return
+                }
+                continuation.resume(returning: data)
+            }
+        }
         
-        let context = container.newBackgroundContext()
-        try await context.perform {
-            let predicate = NSPredicate(format: "faviconURL == %@", url as CVarArg)
-            let request = ZimFile.fetchRequest(predicate: predicate)
-            guard let zimFile = try context.fetch(request).first else { return }
-            zimFile.faviconData = data
-            try context.save()
+        try await withCheckedThrowingContinuation { (continuation: CheckedContinuation<Void, Error>) -> Void in
+            let context = container.newBackgroundContext()
+            context.performAndWait {
+                do {
+                    let predicate = NSPredicate(format: "faviconURL == %@", url as CVarArg)
+                    let request = ZimFile.fetchRequest(predicate: predicate)
+                    guard let zimFile = try context.fetch(request).first else { return }
+                    zimFile.faviconData = data
+                    try context.save()
+                } catch {
+                    continuation.resume(throwing: error)
+                }
+            }
+            continuation.resume()
         }
     }
     
@@ -175,14 +213,14 @@ class Database {
     }
     
     /// Merge changes performed on batch requests to view context.
-    private func mergeChanges() async throws {
+    private func mergeChanges() throws {
         let context = container.newBackgroundContext()
         context.mergePolicy = NSMergePolicy.mergeByPropertyObjectTrump
         context.undoManager = nil
-        try await context.perform {
+        context.perform {
             // fetch and merge changes
             let fetchRequest = NSPersistentHistoryChangeRequest.fetchHistory(after: self.token)
-            guard let result = try context.execute(fetchRequest) as? NSPersistentHistoryResult,
+            guard let result = try? context.execute(fetchRequest) as? NSPersistentHistoryResult,
                   let transactions = result.result as? [NSPersistentHistoryTransaction] else { return }
             self.container.viewContext.perform {
                 transactions.forEach { transaction in
@@ -193,13 +231,13 @@ class Database {
             
             // update token
             guard let token = transactions.last?.token else { return }
-            let data = try NSKeyedArchiver.archivedData(withRootObject: token, requiringSecureCoding: true)
+            let data = try? NSKeyedArchiver.archivedData(withRootObject: token, requiringSecureCoding: true)
             UserDefaults.standard.set(data, forKey: "PersistentHistoryToken")
             
             // purge history
             let sevenDaysAgo = Date(timeIntervalSinceNow: -3600 * 24 * 7)
             let purgeRequest = NSPersistentHistoryChangeRequest.deleteHistory(before: sevenDaysAgo)
-            try context.execute(purgeRequest)
+            _ = try? context.execute(purgeRequest)
         }
     }
 }
