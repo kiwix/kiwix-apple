dc9734c731c805888518ca7b4be08e6f4b1ce477
Chris Li <chris@kiwix.org>
2022-05-27 11:41:33 -0400

    SidebarZimFilesOpened

--- SwiftUI/Reader/SidebarZimFilesOpened.swift
+++ SwiftUI/Reader/SidebarZimFilesOpened.swift
@@ -9,16 +9,27 @@
 import SwiftUI
 
 struct SidebarZimFilesOpened: View {
+    @Binding var url: URL?
     @FetchRequest(
         sortDescriptors: [SortDescriptor(\ZimFile.size, order: .reverse)],
         predicate: NSPredicate(format: "fileURLBookmark != nil"),
         animation: .easeInOut
     ) private var zimFiles: FetchedResults<ZimFile>
-    @State private var selected: ZimFile?
     
     var body: some View {
-        List(zimFiles, id: \.self, selection: $selected) { zimFile in
-            ZimFileRow(zimFile)
+        List(zimFiles.compactMap({ Item($0) }), selection: $url) { item in
+            ZimFileRow(item.zimFile)
+        }
+    }
+    
+    struct Item: Hashable, Identifiable {
+        let id: URL
+        let zimFile: ZimFile
+        
+        init?(_ zimFile: ZimFile) {
+            guard let url = ZimFileService.shared.getMainPageURL(zimFileID: zimFile.fileID) else { return nil }
+            self.id = url
+            self.zimFile = zimFile
         }
     }
 }
