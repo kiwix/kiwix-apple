8122a513bf979c1df62e73893d6fe31fc372e3f3
Chris Li <chris@kiwix.org>
2022-07-31 17:07:39 -0400

    setup

---
+++ SwiftUI/macOS/App.swift
@@ -0,0 +1,24 @@
+//
+//  App.swift
+//  Kiwix
+//
+//  Created by Chris Li on 7/31/22.
+//  Copyright © 2022 Chris Li. All rights reserved.
+//
+
+import SwiftUI
+
+@main
+struct Kiwix: App {
+    init() {
+        LibraryViewModel.reopen()
+    }
+    
+    var body: some Scene {
+        WindowGroup {
+            NavigationView {
+                
+            }
+        }
+    }
+}
