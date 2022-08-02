d956ca1697900d6b06015e64a495df1f93f246c3
Chris Li <chris@kiwix.org>
2022-08-02 09:10:30 -0400

    root view iOS macos

---
+++ SwiftUI/macOS/RootView_iOS.swift
@@ -0,0 +1,133 @@
+//
+//  RootView_iOS.swift
+//  Kiwix for iOS
+//
+//  Created by Chris Li on 8/2/22.
+//  Copyright © 2022 Chris Li. All rights reserved.
+//
+
+import SwiftUI
+
+struct RootView_iOS: UIViewControllerRepresentable {
+    @State private var isSearchActive = false
+    @State private var searchText = ""
+    
+    func makeUIViewController(context: Context) -> UINavigationController {
+        let controller = UIHostingController(rootView: Content(isSearchActive: $isSearchActive))
+        let navigationController = UINavigationController(rootViewController: controller)
+        controller.definesPresentationContext = true
+        
+        // configure search
+        context.coordinator.searchController.delegate = context.coordinator
+        context.coordinator.searchController.searchBar.autocorrectionType = .no
+        context.coordinator.searchController.searchBar.autocapitalizationType = .none
+        context.coordinator.searchController.searchBar.searchBarStyle = .minimal
+        context.coordinator.searchController.hidesNavigationBarDuringPresentation = false
+        context.coordinator.searchController.searchResultsUpdater = context.coordinator
+        context.coordinator.searchController.automaticallyShowsCancelButton = false
+        context.coordinator.searchController.showsSearchResultsController = true
+        context.coordinator.searchController.obscuresBackgroundDuringPresentation = true
+        
+        // configure navigation item
+        controller.navigationItem.titleView = context.coordinator.searchController.searchBar
+        if #available(iOS 15.0, *) {
+            controller.navigationItem.scrollEdgeAppearance = {
+                let apperance = UINavigationBarAppearance()
+                apperance.configureWithDefaultBackground()
+                return apperance
+            }()
+            navigationController.toolbar.scrollEdgeAppearance = {
+                let apperance = UIToolbarAppearance()
+                apperance.configureWithDefaultBackground()
+                return apperance
+            }()
+        }
+        
+        // observe bookmark toggle notification
+        context.coordinator.bookmarkToggleObserver = NotificationCenter.default.addObserver(
+            forName: ReaderViewModel.bookmarkNotificationName, object: nil, queue: nil
+        ) { notification in
+            let isBookmarked = notification.object != nil
+            let hudController = HUDController()
+            hudController.modalPresentationStyle = .custom
+            hudController.transitioningDelegate = hudController
+            hudController.direction = isBookmarked ? .down : .up
+            hudController.imageView.image = isBookmarked ? #imageLiteral(resourceName: "StarAdd") : #imageLiteral(resourceName: "StarRemove")
+            hudController.label.text = isBookmarked ?
+                NSLocalizedString("Added", comment: "Bookmark HUD") :
+                NSLocalizedString("Removed", comment: "Bookmark HUD")
+            controller.present(hudController, animated: true) {
+                DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+                    hudController.dismiss(animated: true, completion: nil)
+                }
+            }
+        }
+        
+        return navigationController
+    }
+    
+    func updateUIViewController(_ uiViewController: UINavigationController, context: Context) {
+        if !isSearchActive {
+            DispatchQueue.main.async {
+                context.coordinator.searchController.isActive = false
+            }
+        }
+    }
+    
+    func makeCoordinator() -> Coordinator { Coordinator(self) }
+    
+    class Coordinator: NSObject, UISearchControllerDelegate, UISearchResultsUpdating {
+        let rootView: RootView_iOS
+        let searchController: UISearchController
+        var bookmarkToggleObserver: NSObjectProtocol?
+        
+        init(_ rootView: RootView_iOS) {
+            self.rootView = rootView
+            let searchResultsController = UIHostingController(rootView: Search(searchText: rootView.$searchText))
+            self.searchController = UISearchController(searchResultsController: searchResultsController)
+            super.init()
+        }
+        
+        func willPresentSearchController(_ searchController: UISearchController) {
+            withAnimation {
+                rootView.isSearchActive = true
+            }
+        }
+        
+        func updateSearchResults(for searchController: UISearchController) {
+            guard rootView.isSearchActive else { return }
+            rootView.searchText = searchController.searchBar.text ?? ""
+        }
+    }
+}
+
+private struct Content: View {
+    @Binding var isSearchActive: Bool
+    @Environment(\.horizontalSizeClass) var horizontalSizeClass
+    @State private var url: URL?
+    @StateObject private var viewModel = ReaderingViewModel()
+    
+    var body: some View {
+        Group {
+            if url == nil {
+                Welcome(url: $url)
+            } else {
+                WebView(url: $url).ignoresSafeArea(.container, edges: .all)
+            }
+        }
+        .environment(\.managedObjectContext, Database.shared.container.viewContext)
+        .environmentObject(viewModel)
+        .toolbar {
+            ToolbarItemGroup(placement: .bottomBar) {
+                if horizontalSizeClass == .compact, !isSearchActive {
+                    Button {
+                        viewModel.webView?.goBack()
+                    } label: { Image(systemName: "chevron.backward") }.disabled(!viewModel.canGoBack)
+                    Button {
+                        viewModel.webView?.goForward()
+                    } label: { Image(systemName: "chevron.forward") }.disabled(!viewModel.canGoForward)
+                }
+            }
+        }
+    }
+}
---
+++ SwiftUI/macOS/RootView_macOS.swift
@@ -0,0 +1,122 @@
+//
+//  RootView_macOS.swift
+//  Kiwix for macOS
+//
+//  Created by Chris Li on 8/2/22.
+//  Copyright © 2022 Chris Li. All rights reserved.
+//
+
+import SwiftUI
+
+struct RootView_macOS: View {
+    @State private var navigationItem: NavigationItem? = .reading
+    @State private var url: URL?
+    @State private var searchText = ""
+    @StateObject private var readingViewModel = ReaderingViewModel()
+    
+    private let primaryNavigationItems: [NavigationItem] = [.reading, .bookmarks, .map]
+    private let libraryNavigationItems: [NavigationItem] = [.opened, .categories, .new, .downloads]
+        
+    var body: some View {
+        NavigationView {
+            List(selection: $navigationItem) {
+                ForEach(primaryNavigationItems, id: \.self) { navigationLink($0) }
+                Section {
+                    ForEach(libraryNavigationItems, id: \.self) { navigationLink($0) }
+                } header: { Text("Library") }
+            }
+            .frame(minWidth: 150)
+            .toolbar { SidebarButton() }
+            EmptyView()  // required so the UI does not look broken on macOS
+        }
+        .environment(\.managedObjectContext, Database.shared.container.viewContext)
+        .onOpenURL { url in
+            if url.isFileURL {
+                guard let metadata = ZimFileService.getMetaData(url: url) else { return }
+                LibraryViewModel.open(url: url)
+                self.url = ZimFileService.shared.getMainPageURL(zimFileID: metadata.fileID)
+            } else if url.scheme == "kiwix" {
+                self.url = url
+            }
+        }
+    }
+    
+    @ViewBuilder
+    private func navigationLink(_ navigationItem: NavigationItem) -> some View {
+        NavigationLink(tag: navigationItem, selection: $navigationItem) {
+            destination(navigationItem)
+        } label: {
+            Label(navigationItem.name, systemImage: navigationItem.icon)
+        }
+    }
+    
+    @ViewBuilder
+    private func destination(_ navigationItem: NavigationItem) -> some View {
+        switch navigationItem {
+        case .reading:
+            ReadingView(url: $url).searchable(text: $searchText).environmentObject(readingViewModel)
+        case .bookmarks:
+            Text(navigationItem.name)
+        case .map:
+            MapView()
+        case .opened:
+            ZimFilesOpened()
+        case .categories:
+            Text(navigationItem.name)
+        case .new:
+            Text(navigationItem.name)
+        case .downloads:
+            Text(navigationItem.name)
+        default:
+            EmptyView()
+        }
+    }
+}
+
+private enum NavigationItem: String, Identifiable, CaseIterable {
+    var id: String { rawValue }
+
+    case reading, bookmarks, map, opened, categories, new, downloads, settings
+
+    var name: String {
+        switch self {
+        case .reading:
+            return "Reading"
+        case .bookmarks:
+            return "Bookmarks"
+        case .map:
+            return "Map"
+        case .settings:
+            return "Settings"
+        case .opened:
+            return "Opened"
+        case .categories:
+            return "Categories"
+        case .new:
+            return "New"
+        case .downloads:
+            return "Downloads"
+        }
+    }
+    
+    var icon: String {
+        switch self {
+        case .reading:
+            return "book"
+        case .bookmarks:
+            return "star"
+        case .map:
+            return "map"
+        case .settings:
+            return "gear"
+        case .opened:
+            return "folder"
+        case .categories:
+            return "books.vertical"
+        case .new:
+            return "newspaper"
+        case .downloads:
+            return "tray.and.arrow.down"
+        }
+    }
+}
