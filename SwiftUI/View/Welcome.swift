dc14d6540a020b2e30b06fbf51fe2b534ce5ccdb
Chris Li <chris@kiwix.org>
2022-08-06 15:02:02 -0400

    welcome no content

--- SwiftUI/View/Welcome.swift
+++ SwiftUI/View/Welcome.swift
@@ -23,8 +23,23 @@ struct Welcome: View {
     @State private var selectedZimFile: ZimFile?
     
     var body: some View {
-        if zimFiles.isEmpty {
-            
+        if !zimFiles.isEmpty {
+            VStack(alignment: .leading, spacing: 10) {
+                HStack {
+                    Image("Kiwix_logo_v3")
+                        .resizable()
+                        .frame(width: 35, height: 35)
+                        .padding(2)
+                        .background(RoundedRectangle(cornerRadius: 10).foregroundColor(.white))
+                    Text("Kiwix").font(.largeTitle).fontWeight(.bold)
+                }
+                Divider()
+                Text("Quick start:").font(.headline)
+                Button("Open") {
+                    
+                }.buttonStyle(.borderedProminent)
+                Spacer()
+            }.padding(50)
         } else {
             ScrollView {
                 LazyVGrid(
@@ -46,9 +61,9 @@ struct Welcome: View {
                         Text("Main Page").font(.title3).fontWeight(.semibold)
                     }
                     Section {
-                        ForEach(bookmarks[...10]) { bookmark in
+                        ForEach(bookmarks) { bookmark in
                             Button { url = bookmark.articleURL } label: {
-                                ArticleCell(bookmark: bookmark).frame(height: itemHeight)
+                                ArticleCell(bookmark: bookmark).frame(height: bookmarkItemHeight)
                             }.buttonStyle(.plain)
                         }
                     } header: {
@@ -64,11 +79,18 @@ struct Welcome: View {
         }
     }
     
-    private var itemHeight: CGFloat? {
+    private var bookmarkItemHeight: CGFloat? {
         #if os(macOS)
         82
         #elseif os(iOS)
         horizontalSizeClass == .regular ? 110: nil
         #endif
+    }
+}
+
+struct WelcomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        Welcome(url: .constant(nil)).preferredColorScheme(.light).padding()
+        Welcome(url: .constant(nil)).preferredColorScheme(.dark).padding()
     }
 }
