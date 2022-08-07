3b32574824fe946ce5544d8d032daa9b17507923
Chris Li <chris@kiwix.org>
2022-08-07 17:30:02 -0400

    buttons

--- SwiftUI/View/BuildingBlocks/Buttons.swift
+++ SwiftUI/View/BuildingBlocks/Buttons.swift
@@ -1,5 +1,5 @@
 //
-//  NavigationButtons.swift
+//  Buttons.swift
 //  Kiwix
 //
 //  Created by Chris Li on 2/13/22.
@@ -249,3 +249,17 @@ struct RandomArticleMenu: View {
         }.help("Show random article")
     }
 }
+
+#if os(macOS)
+struct SidebarButton: View {
+    var body: some View {
+        Button {
+            guard let responder = NSApp.keyWindow?.firstResponder else { return }
+            responder.tryToPerform(#selector(NSSplitViewController.toggleSidebar(_:)), with: nil)
+        } label: {
+            Image(systemName: "sidebar.leading")
+        }
+        .help("Show sidebar")
+    }
+}
+#endif
