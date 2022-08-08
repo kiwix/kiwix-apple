2a3311600a448aa5c211989af421979b34dc41f4
Chris Li <chris@kiwix.org>
2022-08-08 09:12:15 -0400

    CategoryList

--- SwiftUI/Library/LibraryCategory.swift
+++ SwiftUI/Library/LibraryCategory.swift
@@ -8,11 +8,13 @@
 
 import SwiftUI
 
+import Defaults
+
 struct LibraryCategories: View {
     @State private var selected: Category = .wikipedia
     
     var body: some View {
-        LibraryCategory(selectedCategory: $selected)
+        LibraryCategory(category: $selected)
             .navigationTitle(NavigationItem.categories.name)
             .toolbar {
                 Picker("Category", selection: $selected) {
@@ -24,15 +26,103 @@ struct LibraryCategories: View {
 
 
 struct LibraryCategory: View {
-    @Binding var selectedCategory: Category
+    @Binding var category: Category
     @State private var searchText = ""
-    @State private var selectedZimFile: ZimFile?
+    
+    var body: some View {
+        if category == .ted || category == .stackExchange || category == .other {
+            CategoryList(category: $category, searchText: $searchText)
+        } else {
+            CategoryGrid(searchText: $searchText, category: category)
+        }
+//        .modifier(Searchable(searchText: $searchText))
+//        .modifier(ZimFileDetailPanel(zimFile: selectedZimFile))
+    }
+}
+
+private struct CategoryGrid: View {
+    @Binding var searchText: String
+    
+    let category: Category
+    
+    var body: some View {
+        Text(category.name)
+    }
+}
+
+private struct CategoryList: View {
+    @Binding var category: Category
+    @Binding var searchText: String
+    @Default(.libraryLanguageCodes) private var languageCodes
+    @FetchRequest private var zimFiles: FetchedResults<ZimFile>
+    @State private var selected: ZimFile?
+    
+    init(category: Binding<Category>, searchText: Binding<String>) {
+        self._category = category
+        self._searchText = searchText
+        self._zimFiles = FetchRequest<ZimFile>(
+            sortDescriptors: [
+                NSSortDescriptor(
+                    key: "name", ascending: true, selector: #selector(NSString.localizedCaseInsensitiveCompare)
+                ),
+                NSSortDescriptor(keyPath: \ZimFile.size, ascending: false)
+            ],
+            predicate: CategoryList.buildPredicate(
+                category: category.wrappedValue, searchText: searchText.wrappedValue
+            ),
+            animation: .easeInOut
+        )
+    }
     
     var body: some View {
         Group {
-            Text(selectedCategory.name)
+            if zimFiles.isEmpty {
+                Message(text: "No zim file under this category.")
+            } else {
+                List(zimFiles, id: \.self, selection: $selected) { zimFile in
+                    ZimFileRow(zimFile)
+                        .modifier(ZimFileContextMenu(selected: $selected, zimFile: zimFile))
+                        .modifier(ZimFileSelection(selected: $selected, zimFile: zimFile))
+                }
+            }
         }
+        .modifier(ListStyle())
+        .modifier(ZimFileDetailPanel(zimFile: selected))
         .modifier(Searchable(searchText: $searchText))
-        .modifier(ZimFileDetailPanel(zimFile: selectedZimFile))
+        .onChange(of: category) { _ in selected = nil }
+        .onChange(of: searchText) { _ in
+            if #available(iOS 15.0, *) {
+                zimFiles.nsPredicate = CategoryList.buildPredicate(category: category, searchText: searchText)
+            }
+        }
+        .onChange(of: languageCodes) { _ in
+            if #available(iOS 15.0, *) {
+                zimFiles.nsPredicate = CategoryList.buildPredicate(category: category, searchText: searchText)
+            }
+        }
+    }
+    
+    private static func buildPredicate(category: Category, searchText: String) -> NSPredicate {
+        var predicates = [
+            NSPredicate(format: "category == %@", category.rawValue),
+            NSPredicate(format: "languageCode IN %@", Defaults[.libraryLanguageCodes]),
+            NSPredicate(format: "requiresServiceWorkers == false")
+        ]
+        if !searchText.isEmpty {
+            predicates.append(NSPredicate(format: "name CONTAINS[cd] %@", searchText))
+        }
+        return NSCompoundPredicate(andPredicateWithSubpredicates: predicates)
+    }
+    
+    struct ListStyle: ViewModifier {
+        func body(content: Content) -> some View {
+            #if os(macOS)
+            content.listStyle(.inset)
+            #elseif os(iOS)
+            content.listStyle(.plain)
+            #endif
+        }
     }
 }
+
+
