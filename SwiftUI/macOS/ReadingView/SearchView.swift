7128af302024f92f27a2d33229404ea790bd899b
Chris Li <chris@kiwix.org>
2022-08-21 15:05:16 -0400

    search result

--- SwiftUI/macOS/ReadingView/SearchView.swift
+++ SwiftUI/macOS/ReadingView/SearchView.swift
@@ -9,36 +9,63 @@
 import SwiftUI
 
 struct SearchView: View {
+    @Environment(\.managedObjectContext) private var managedObjectContext
+    @FetchRequest(
+        sortDescriptors: [NSSortDescriptor(keyPath: \ZimFile.size, ascending: false)],
+        predicate: ZimFile.withFileURLBookmarkPredicate,
+        animation: .easeInOut
+    ) private var zimFiles: FetchedResults<ZimFile>
+    @State private var isShowingMoreRecentSearch = false
+    @State private var selectedRecentSearchText: String?
+    
     var body: some View {
         GeometryReader { proxy in
-            if proxy.size.width > 425 {
-                HStack {
-                    Spacer()
-                    content
-                        .background(Material.thin)
-                        .cornerRadius(10)
-                        .overlay(
-                            RoundedRectangle(cornerRadius: 10)
-                                .strokeBorder(Color.secondary.opacity(0.2), lineWidth: 0.5)
-                        )
-                        .shadow(color: Color.black.opacity(0.2), radius: 3, x: 0, y: 2)
-                        .frame(width: 400, height: min(proxy.size.height * 0.8, 800))
-                        .padding(8)
-                }
-            } else {
-                content.background(Color.background)
+            ZStack(alignment: .topTrailing) {
+                Color.clear
+                content
+                    .background(Material.regular)
+                    .cornerRadius(10)
+                    .shadow(color: Color.black.opacity(0.2), radius: 3, x: 0, y: 2)
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 10)
+                            .strokeBorder(Color.secondary.opacity(0.2), lineWidth: 0.5)
+                    )
+                    .frame(width: min(proxy.size.width * 0.75, 425), height: min(proxy.size.height * 0.8, 600))
+                    .padding(8)
             }
         }
     }
     
     var content: some View {
-        ScrollView {
-            LazyVStack {
-                Text("result 1")
-                Text("result 2")
-                Text("result 3")
+        List(selection: $selectedRecentSearchText) {
+            recentSearch
+            filter
+        }.background(Color.green)
+    }
+    
+    var recentSearch: some View {
+        Section {
+            LazyVGrid(columns: [GridItem(.adaptive(minimum: 100))]) {
+                ForEach(1..<13) { index in
+                    Button("recent search item \(index)") {
+                        
+                    }
+                }
             }
-        }
+        } header: { Text("Recent Search") }
+    }
+    
+    var filter: some View {
+        Section {
+            ForEach(zimFiles) { zimFile in
+                Toggle(zimFile.name, isOn: Binding<Bool>(get: {
+                    zimFile.includedInSearch
+                }, set: {
+                    zimFile.includedInSearch = $0
+                    try? managedObjectContext.save()
+                }))
+            }
+        } header: { Text("Included in Search") }
     }
 }
 
