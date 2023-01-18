1af0b4be7d45fddf50584f5cf6ed6dced92da114
Chris Li <chris@kiwix.org>
2023-01-18 17:34:22 -0500

    LibraryRefreshViewModelTest

--- Tests/LibraryRefreshViewModelTest.swift
+++ Tests/LibraryRefreshViewModelTest.swift
@@ -48,11 +48,12 @@ final class LibraryRefreshViewModelTest: XCTestCase {
         await viewModel.start()
         XCTAssert(viewModel.error is LibraryRefreshError)
         XCTAssertEqual(
-            viewModel.error!.localizedDescription,
+            viewModel.error?.localizedDescription,
             "Error retrieving library data. The operation couldn’t be completed. (NSURLErrorDomain error -1001.)"
         )
     }
 
+    /// Test fetching library data response contains non-success status code.
     func testFetchBadStatusCode() async {
         HTTPTestingURLProtocol.handler = { urlProtocol in
             let response = HTTPURLResponse(
@@ -67,7 +68,29 @@ final class LibraryRefreshViewModelTest: XCTestCase {
         await viewModel.start()
         XCTAssert(viewModel.error is LibraryRefreshError)
         XCTAssertEqual(
-            viewModel.error!.localizedDescription,
+            viewModel.error?.localizedDescription,
+            "Error retrieving library data. HTTP Status 404."
+        )
+    }
+    
+    /// Test OPDS data is invalid.
+    func testInvalidOPDSData() async {
+        HTTPTestingURLProtocol.handler = { urlProtocol in
+            let response = HTTPURLResponse(
+                url: URL(string: "https://library.kiwix.org/catalog/root.xml")!,
+                statusCode: 200, httpVersion: nil, headerFields: [:]
+            )!
+            urlProtocol.client?.urlProtocol(urlProtocol, didLoad: "Invalid OPDS Data".data(using: .utf8)!)
+            urlProtocol.client?.urlProtocol(urlProtocol, didReceive: response, cacheStoragePolicy: .notAllowed)
+            urlProtocol.client?.urlProtocolDidFinishLoading(urlProtocol)
+        }
+        
+        let viewModel = LibraryRefreshViewModel(urlSession: urlSession)
+        await viewModel.start()
+        
+        XCTExpectFailure("Requires work in dependency to resolve the issue.")
+        XCTAssertEqual(
+            viewModel.error?.localizedDescription,
             "Error retrieving library data. HTTP Status 404."
         )
     }
