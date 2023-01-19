ab43619992c4f86a751b3a8f94fdcfd5be9c403c
Chris Li <chris@kiwix.org>
2023-01-18 21:25:06 -0500

    code

--- SwiftUI/ViewModels/LibraryViewModel.swift
+++ SwiftUI/ViewModels/LibraryViewModel.swift
@@ -15,16 +15,27 @@ public class LibraryRefreshViewModel: ObservableObject {
     @Published public private(set) var error: Error?
     
     private let urlSession: URLSession
+    private let context: NSManagedObjectContext
+    private var insertionCount = 0
+    private var deletionCount = 0
     
-    public init(urlSession: URLSession? = nil) {
+    public init(urlSession: URLSession? = nil, context: NSManagedObjectContext? = nil) {
         self.urlSession = urlSession ?? URLSession.shared
+        self.context = context ?? {
+            let context = NSManagedObjectContext(concurrencyType: .privateQueueConcurrencyType)
+            context.persistentStoreCoordinator = Database.shared.container.persistentStoreCoordinator
+            context.mergePolicy = NSMergePolicy.mergeByPropertyObjectTrump
+            context.undoManager = nil
+            return context
+        }()
     }
     
     public func start() async {
         do {
             defer { self.error = nil }
             guard let data = try await fetchData() else { return }
-            try await process(data: data)
+            let parser = try await parse(data: data)
+            try await process(parser)
         } catch {
             self.error = error
         }
@@ -39,8 +50,10 @@ public class LibraryRefreshViewModel: ObservableObject {
             guard response.statusCode == 200 else {
                 throw LibraryRefreshError.retrieve(description: "HTTP Status \(response.statusCode).")
             }
+            os_log("Retrieved OPDS Data, size: %llu bytes", log: Log.OPDS, type: .info, data.count)
             return data
         } catch {
+            os_log("Error retrieving OPDS Data: %s", log: Log.OPDS, type: .error)
             if let error = error as? LibraryRefreshError {
                 throw error
             } else {
@@ -49,14 +62,78 @@ public class LibraryRefreshViewModel: ObservableObject {
         }
     }
     
-    private func process(data: Data) async throws -> OPDSParser {
+    private func parse(data: Data) async throws -> OPDSParser {
         try await withCheckedThrowingContinuation { continuation in
             DispatchQueue.global().async {
                 let parser = OPDSParser()
-                do { try parser.parse(data: data) } catch { continuation.resume(throwing: error) }
-                continuation.resume(returning: parser)
+                do {
+                    try parser.parse(data: data)
+                    continuation.resume(returning: parser)
+                } catch {
+                    continuation.resume(throwing: error)
+                }
             }
         }
+    }
+    
+    private func process(_ parser: OPDSParser) async throws {
+        try await withCheckedThrowingContinuation { continuation in
+            context.perform {
+                do {
+                    // insert new zim files
+                    let existing = try self.context.fetch(ZimFile.fetchRequest()).map { $0.fileID }
+                    var zimFileIDs = parser.zimFileIDs.subtracting(existing)
+                    let insertRequest = NSBatchInsertRequest(
+                        entity: ZimFile.entity(),
+                        managedObjectHandler: { zimFile in
+                            guard let zimFile = zimFile as? ZimFile else { return true }
+                            while !zimFileIDs.isEmpty {
+                                guard let id = zimFileIDs.popFirst(),
+                                      let metadata = parser.getMetaData(id: id) else { continue }
+                                LibraryOperations.configureZimFile(zimFile, metadata: metadata)
+                                return false
+                            }
+                            return true
+                        }
+                    )
+                    insertRequest.resultType = .count
+                    if let result = try self.context.execute(insertRequest) as? NSBatchInsertResult {
+                        self.insertionCount = result.result as? Int ?? 0
+                    }
+                    
+                    // delete old zim files
+                    let fetchRequest: NSFetchRequest<NSFetchRequestResult> = ZimFile.fetchRequest()
+                    fetchRequest.predicate = NSCompoundPredicate(andPredicateWithSubpredicates: [
+                        NSPredicate(format: "fileURLBookmark == nil"),
+                        NSPredicate(format: "NOT fileID IN %@", parser.zimFileIDs)
+                    ])
+                    let deleteRequest = NSBatchDeleteRequest(fetchRequest: fetchRequest)
+                    deleteRequest.resultType = .resultTypeCount
+                    if let result = try self.context.execute(deleteRequest) as? NSBatchDeleteResult {
+                        self.deletionCount = result.result as? Int ?? 0
+                    }
+                    
+                    continuation.resume()
+                } catch {
+                    os_log("Error saving OPDS Data: %s", log: Log.OPDS, type: .error, error.localizedDescription)
+                    continuation.resume(throwing: LibraryRefreshError.process)
+                }
+            }
+        }
+    }
+    
+    private func postProcess() {
+        // update library last refresh timestamp
+        Defaults[.libraryLastRefresh] = Date()
+        
+        // populate library language code if there isn't one set already
+        if Defaults[.libraryLanguageCodes].isEmpty, let currentLanguageCode = Locale.current.languageCode {
+            Defaults[.libraryLanguageCodes] = [currentLanguageCode]
+        }
+        
+        // logging
+        os_log("Refresh finished -- addition: %d, deletion: %d",
+               log: Log.OPDS, type: .default, insertionCount, deletionCount)
     }
 }
 
