cba260d14e81fd252b6cfe1e722adec1bba5bd62
Chris Li <chris@kiwix.org>
2023-07-01 10:01:25 -0400

    browser

--- Browser/WebView.swift
+++ Browser/WebView.swift
@@ -14,29 +14,13 @@ import Defaults
 
 #if os(macOS)
 struct WebView: NSViewRepresentable {
-    @Binding var url: URL?
-    @EnvironmentObject var viewModel: ViewModel
-    @EnvironmentObject var readingViewModel: ReadingViewModel
+    @EnvironmentObject private var viewModel: BrowserViewModel
     
     func makeNSView(context: Context) -> WKWebView {
-        let webView = readingViewModel.webView
-        webView.allowsBackForwardNavigationGestures = true
-        webView.configuration.userContentController.add(readingViewModel, name: "headings")
-        webView.navigationDelegate = viewModel
-        context.coordinator.setupObservers(webView)
-        return webView
+        viewModel.webView
     }
     
-    func updateNSView(_ webView: WKWebView, context: Context) {
-        guard let url = url, webView.url?.absoluteString != url.absoluteString else { return }
-        webView.load(URLRequest(url: url))
-    }
-    
-    static func dismantleNSView(_ webView: WKWebView, coordinator: WebViewCoordinator) {
-        webView.configuration.userContentController.removeScriptMessageHandler(forName: "headings")
-    }
-    
-    func makeCoordinator() -> WebViewCoordinator { WebViewCoordinator(self) }
+    func updateNSView(_ webView: WKWebView, context: Context) { }
 }
 #elseif os(iOS)
 struct WebView: UIViewControllerRepresentable {
@@ -62,15 +46,32 @@ struct WebView: UIViewControllerRepresentable {
 class WebViewController: UIViewController {
     let tabID: UUID
     let webView: WKWebView
+    private var zoomScale: CGFloat = 1
     
     init(tabID: UUID, webView: WKWebView) {
         self.tabID = tabID
         self.webView = webView
         super.init(nibName: nil, bundle: nil)
+        NotificationCenter.default.addObserver(
+            self,
+            selector: #selector(sceneDidEnterBackground),
+            name: UIScene.didEnterBackgroundNotification,
+            object: nil
+        )
+        NotificationCenter.default.addObserver(
+            self,
+            selector: #selector(sceneWillEnterForeground),
+            name: UIScene.willEnterForegroundNotification,
+            object: nil
+        )
     }
 
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    deinit {
+        NotificationCenter.default.removeObserver(self)
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -83,11 +84,37 @@ class WebViewController: UIViewController {
         webView.setValue(view.safeAreaInsets, forKey: "_obscuredInsets")
     }
     
-    /// Install web view, and setup property observers
+    
+    /// Store page zoom scale when scene enters background
+    /// HACK: when scene enters background, the system resizes the scene and take various screenshots (for app switcher), during which
+    /// the webview becomes zoomed in. To mitigate, store and reapply webview's zoom scale when scene enters backgroud / foreground.
+    @objc private func sceneDidEnterBackground() {
+        zoomScale = webView.scrollView.zoomScale
+    }
+    
+    /// Reapply stored zoom scale when scene enters foreground
+    @objc private func sceneWillEnterForeground() {
+        self.webView.alpha = 0
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
+            self.webView.scrollView.setZoomScale(self.zoomScale, animated: false)
+            UIView.animate(withDuration: 0.1) {
+                self.webView.alpha = 1
+            }
+        }
+    }
+    
+    /// Install web view
     private func setupWebView() {
         guard !view.subviews.contains(webView) else { return }
         webView.translatesAutoresizingMaskIntoConstraints = false
         view.addSubview(webView)
+        
+        /*
+         HACK: Make sure the webview content does not jump after state restoration
+         It appears the webview's state restoration does not properly take into account of the content inset.
+         To mitigate, first pin the webview's top against safe area top anchor, let the webview restore state,
+         then pin the webview's top against view's top anchor, so that content does not appears to move up.
+         */
         
         NSLayoutConstraint.activate([
             view.leftAnchor.constraint(equalTo: webView.leftAnchor),
@@ -97,8 +124,9 @@ class WebViewController: UIViewController {
         
         let topSafeAreaConstraint = view.safeAreaLayoutGuide.topAnchor.constraint(equalTo: webView.topAnchor)
         topSafeAreaConstraint.isActive = true
-        
+                
         DispatchQueue.main.asyncAfter(deadline: .now() + 0.25) {
+            guard self.webView.superview == self.view else { return }
             topSafeAreaConstraint.isActive = false
             let topConstraint = self.view.topAnchor.constraint(equalTo: self.webView.topAnchor)
             topConstraint.isActive = true
