dfa0d8901f4ce6bd45f5d1de093123e11e1d0d0d
Balazs Perlaki-Horvath <ba.perlaki@gmail.com>
2024-01-18 23:53:55 +0100

    Remove grid section preview, it needs no translations

--- Views/BuildingBlocks/GridSection.swift
+++ Views/BuildingBlocks/GridSection.swift
@@ -29,8 +29,8 @@ struct GridSection<Content: View>: View {
 
 struct GridSection_Previews: PreviewProvider {
     static var previews: some View {
-        GridSection(title: "grid_section.preview.title".localized) {
-            Text("grid_section.preview.content".localized)
+        GridSection(title: "Header Text") {
+            Text("Content")
         }
     }
 }
