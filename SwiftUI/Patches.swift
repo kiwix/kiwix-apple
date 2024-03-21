82befde685f46ae2c1b905981a186ea9f9abb07f
Valentin Vila <vilavalentin@gmail.com>
2024-03-21 13:16:56 -0300

    duplicate foregroundColor

--- SwiftUI/Patches.swift
+++ SwiftUI/Patches.swift
@@ -58,7 +58,6 @@ extension Color {
     static let background = Color(UIColor.systemBackground)
     static let secondaryBackground = Color(UIColor.secondarySystemBackground)
     static let tertiaryBackground = Color(UIColor.tertiarySystemBackground)
-    static let systemTextColor = Color(NSColor.textColor)
     #endif
 }
 
