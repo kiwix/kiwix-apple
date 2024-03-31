d6c23073aa4564a987c8ba27e380424f16c7596b
Emmanuel Engelhart <kelson@kiwix.org>
2024-03-31 15:52:30 +0200

    Add GPL headers

--- SwiftUI/Model/DirectoryMonitor.swift
+++ SwiftUI/Model/DirectoryMonitor.swift
@@ -1,4 +1,21 @@
 /*
+ * This file is part of Kiwix for iOS & macOS.
+ *
+ * Kiwix is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * any later version.
+ *
+ * Kiwix is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Kiwix; If not, see https://www.gnu.org/licenses/.
+*/
+
+/*
 Copyright (C) 2015 Apple Inc. All Rights Reserved.
 See LICENSE.txt for this sample’s licensing information
 
