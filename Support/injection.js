dd05ae8604d1bbfb8718d8ab31030accf96c701e
Balazs Perlaki-Horvath <ba.perlaki@gmail.com>
2024-05-19 09:16:48 +0200

    Fix missing handler case in Javascript

--- Support/injection.js
+++ Support/injection.js
@@ -31,7 +31,10 @@ let observer = new IntersectionObserver(function(entries) {
 // register scroll view to handle heading on top of the page
 window.onscroll = function() {
 	if (document.documentElement.scrollTop <= 0) {
-		window.webkit.messageHandlers.headingVisible.postMessage({id: headings[0].id})
+        const handler = window.webkit.messageHandlers.headingVisible
+        if (handler !== undefined) {
+            handler.postMessage({id: headings[0].id})
+        }
 	}
 }
 
