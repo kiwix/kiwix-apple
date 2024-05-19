fc50b16664af2d6328ca9b00fb5b23fb635b4d9e
Balazs Perlaki-Horvath <ba.perlaki@gmail.com>
2024-05-19 21:48:14 +0200

    Re-indent

--- Support/injection.js
+++ Support/injection.js
@@ -30,12 +30,12 @@ let observer = new IntersectionObserver(function(entries) {
 
 // register scroll view to handle heading on top of the page
 window.onscroll = function() {
-	if (document.documentElement.scrollTop <= 0) {
+    if (document.documentElement.scrollTop <= 0) {
         const handler = window.webkit.messageHandlers.headingVisible
         if (handler !== undefined) {
             handler.postMessage({id: headings[0].id})
         }
-	}
+    }
 }
 
 // expand all detail tags
