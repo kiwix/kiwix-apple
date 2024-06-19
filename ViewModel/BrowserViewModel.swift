ea2239d83ab7afff03bf0c9dc1672645ebf2ba16
Balazs Perlaki-Horvath <ba.perlaki@gmail.com>
2024-06-19 18:58:42 +0200

    Fixlint

--- ViewModel/BrowserViewModel.swift
+++ ViewModel/BrowserViewModel.swift
@@ -159,7 +159,6 @@ final class BrowserViewModel: NSObject, ObservableObject,
         }
     }
 
-
     /// Get the webpage in a binary format
     /// - Returns: PDF of the current page (if text type) or binary data of the content
     func pdfData() async -> Data? {
