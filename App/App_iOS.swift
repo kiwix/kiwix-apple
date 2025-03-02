659d8ee6aa042114749173d9153711c92a57178b
Balazs Perlaki-Horvath <ba.perlaki@gmail.com>
2025-03-02 01:27:29 +0100

    Remove live activities from release 3.8.0

--- App/App_iOS.swift
+++ App/App_iOS.swift
@@ -25,17 +25,17 @@ struct Kiwix: App {
     @UIApplicationDelegateAdaptor(AppDelegate.self) private var appDelegate
 
     private let fileMonitor: DirectoryMonitor
-    private let activityService: ActivityService?
+//    private let activityService: ActivityService?
 
     init() {
         fileMonitor = DirectoryMonitor(url: URL.documentDirectory) { LibraryOperations.scanDirectory($0) }
         // MARK: - live activities
-        switch AppType.current {
-        case .kiwix:
-            activityService = ActivityService()
-        case .custom:
-            activityService = nil
-        }
+//        switch AppType.current {
+//        case .kiwix:
+//            activityService = ActivityService()
+//        case .custom:
+//            activityService = nil
+//        }
         UNUserNotificationCenter.current().delegate = appDelegate
         // MARK: - migrations
         if !ProcessInfo.processInfo.arguments.contains("testing") {
@@ -85,7 +85,7 @@ struct Kiwix: App {
                         LibraryOperations.scanDirectory(URL.documentDirectory)
                         LibraryOperations.applyFileBackupSetting()
                         DownloadService.shared.restartHeartbeatIfNeeded()
-                        activityService?.start()
+//                        activityService?.start()
                     case let .custom(zimFileURL):
                         await LibraryOperations.open(url: zimFileURL)
                         ZimMigration.forCustomApps()
