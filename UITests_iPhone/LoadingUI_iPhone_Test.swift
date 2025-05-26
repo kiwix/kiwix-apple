120802b629ad2daeb59cd7642d420043710a6d75
Balazs Perlaki-Horvath <ba.perlaki@gmail.com>
2025-05-26 22:57:14 +0200

    Move existing iOS UI test under iPhone

---
+++ UITests_iPhone/LoadingUI_iPhone_Test.swift
@@ -0,0 +1,51 @@
+// This file is part of Kiwix for iOS & macOS.
+//
+// Kiwix is free software; you can redistribute it and/or modify it
+// under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 3 of the License, or
+// any later version.
+//
+// Kiwix is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+// General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Kiwix; If not, see https://www.gnu.org/licenses/.
+
+import XCTest
+
+final class LoadingUI_iPhone_Test: XCTestCase {
+
+    @MainActor
+    func testLaunchingApp_onIPhone() throws {
+        let app = XCUIApplication()
+        app.launchArguments = ["ui_testing"]
+        app.activate()
+        app /*@START_MENU_TOKEN@*/.buttons[
+            "Library"
+        ] /*[[".otherElements.buttons[\"Library\"]",".buttons[\"Library\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/
+            .tap()
+        app /*@START_MENU_TOKEN@*/.buttons[
+            "New"
+        ] /*[[".tabBars",".buttons[\"New\"]",".buttons[\"newspaper\"]"],[[[-1,2],[-1,1],[-1,0,1]],[[-1,2],[-1,1]]],[1]]@END_MENU_TOKEN@*/
+            .tap()
+        app /*@START_MENU_TOKEN@*/.buttons[
+            "Downloads"
+        ] /*[[".tabBars",".buttons[\"Downloads\"]",".buttons[\"tray.and.arrow.down\"]"],[[[-1,2],[-1,1],[-1,0,1]],[[-1,2],[-1,1]]],[1]]@END_MENU_TOKEN@*/
+            .tap()
+        app /*@START_MENU_TOKEN@*/.buttons[
+            "Opened"
+        ] /*[[".tabBars",".buttons[\"Opened\"]",".buttons[\"folder\"]"],[[[-1,2],[-1,1],[-1,0,1]],[[-1,2],[-1,1]]],[1]]@END_MENU_TOKEN@*/
+            .tap()
+        app /*@START_MENU_TOKEN@*/.buttons[
+            "Categories"
+        ] /*[[".tabBars",".buttons[\"Categories\"]",".buttons[\"books.vertical\"]"],[[[-1,2],[-1,1],[-1,0,1]],[[-1,2],[-1,1]]],[1]]@END_MENU_TOKEN@*/
+            .tap()
+        app /*@START_MENU_TOKEN@*/.buttons[
+            "Done"
+        ] /*[[".otherElements[\"Done\"].buttons.firstMatch",".otherElements.buttons[\"Done\"]",".buttons[\"Done\"]"],[[[-1,2],[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/
+            .tap()
+
+    }
+}
