660e269858087c9aad6547c30c89994a65d7d6aa
Balazs Perlaki-Horvath <ba.perlaki@gmail.com>
2025-08-23 18:20:09 +0200

    Apply OpenSettings to macOS only

--- Views/ViewModifiers/OpenSettingsModifier.swift
+++ Views/ViewModifiers/OpenSettingsModifier.swift
@@ -13,6 +13,7 @@
 // You should have received a copy of the GNU General Public License
 // along with Kiwix; If not, see https://www.gnu.org/licenses/.
 
+#if os(macOS)
 import SwiftUI
 
 @available(macOS 14, *)
@@ -56,3 +57,4 @@ struct OpeningSettingsModifier_macOS_13: ViewModifier {
         }
     }
 }
+#endif
