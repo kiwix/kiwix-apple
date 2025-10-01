1062fd1a1e5570438866e17c49f9d9e91c989673
Balazs Perlaki-Horvath <ba.perlaki@gmail.com>
2025-10-01 22:16:01 +0200

    Initial UI

---
+++ Views/Settings/DiagnosticsView.swift
@@ -0,0 +1,52 @@
+// This file is part of Kiwix for iOS & macOS.
+//
+// Kiwix is free software; you can redistribute it and/or modify it
+// under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 3 of the License, or
+// any later version.
+//
+// Kiwix is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+// General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Kiwix; If not, see https://www.gnu.org/licenses/.
+
+import SwiftUI
+
+struct DiagnosticsView: View {
+    
+    @State private var languageSettings = true
+    @State private var applicationLogs = true
+    @State private var zimFiles = true
+    @State private var deviceDetails = true
+    @State private var fileSystem = true
+    
+    var body: some View {
+        VStack {
+            Text("""
+                    Please share the following details, so we can diagnose the problem.
+                    """)
+            .font(.headline)
+            
+            Toggle("Your language settings", isOn: $languageSettings)
+            Toggle("Application logs", isOn: $applicationLogs)
+            Toggle("List of your ZIM files", isOn: $zimFiles)
+            Toggle("Device details", isOn: $deviceDetails)
+            Toggle("File system details", isOn: $fileSystem)
+            
+            AsyncButton {
+                await Diagnostics.entries()
+            } label: {
+                Label("Share", systemImage: "square.and.arrow.up")
+            }
+            .buttonStyle(.borderless)
+            .padding()
+        }
+        .frame(maxWidth: 400)
+        .navigationTitle("Diagnostic Report")
+        .navigationBarTitleDisplayMode(.large)
+        .padding(.horizontal, 40)
+    }
+}
--- Views/Settings/Settings.swift
+++ Views/Settings/Settings.swift
@@ -299,12 +299,6 @@ struct Settings: View {
                 }
             }
             
-            Button("Report a bug") {
-                Task {
-                    await Diagnostics.entries()
-                }
-            }
-            
             Button(LocalString.settings_miscellaneous_button_feedback) {
                 UIApplication.shared.open(URL(string: "mailto:feedback@kiwix.org")!)
             }
@@ -313,6 +307,7 @@ struct Settings: View {
                               appStoreID: Brand.appStoreId)
                 UIApplication.shared.open(url)
             }
+            NavigationLink("Share a diagnostic report") { DiagnosticsView() }
             NavigationLink(LocalString.settings_miscellaneous_navigation_about) { About() }
         }
     }
