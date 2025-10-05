987d8f273d1792c64c484716ad05aff7379ec2bc
Balazs Perlaki-Horvath <ba.perlaki@gmail.com>
2025-10-06 00:10:03 +0200

    Adjust logging for ZIM files and langauges

--- Views/Settings/DiagnosticsView.swift
+++ Views/Settings/DiagnosticsView.swift
@@ -17,48 +17,45 @@ import SwiftUI
 
 struct DiagnosticsView: View {
     
-    @State private var languageSettings = true
-    @State private var applicationLogs = true
-    @State private var zimFiles = true
-    @State private var deviceDetails = true
-    @State private var fileSystem = true
-    
-    #if os(iOS)
+#if os(iOS)
     private var alignment: HorizontalAlignment = .center
-    #else
+#else
     private var alignment: HorizontalAlignment = .leading
-    #endif
+#endif
     
     var body: some View {
-        VStack(alignment: alignment) {
+        VStack(alignment: alignment, spacing: 16) {
+            Spacer()
             Text("""
-                    Please share the following details, so we can diagnose the problem.
-                    """)
+                        Please share the following details, so we can diagnose the problem.
+                        """)
             .font(.headline)
             
-            Toggle("Your language settings", isOn: $languageSettings)
-            Toggle("Application logs", isOn: $applicationLogs)
-            Toggle("List of your ZIM files", isOn: $zimFiles)
-            Toggle("Device details", isOn: $deviceDetails)
-            Toggle("File system details", isOn: $fileSystem)
+            Text("Your language settings")
+            Text("Application logs")
+            Text("List of your ZIM files")
+            Text("Device details")
+            Text("File system details")
+            
             
             AsyncButton {
                 await Diagnostics.entries()
             } label: {
                 Label("Share", systemImage: "square.and.arrow.up")
             }
-            #if os(iOS)
+#if os(iOS)
             .buttonStyle(.borderless)
-            #endif
             .padding()
+#endif
+            Spacer()
         }
         .frame(maxWidth: 400)
         .navigationTitle("Diagnostic Report")
-        #if os(iOS)
+#if os(iOS)
         .navigationBarTitleDisplayMode(.large)
-        #else
-        .tabItem { Label("Diagnostics", systemImage: "exclamationmark.bubble") }
-        #endif
         .padding(.horizontal, 40)
+#else
+        .tabItem { Label("Diagnostics", systemImage: "exclamationmark.bubble") }
+#endif
     }
 }
