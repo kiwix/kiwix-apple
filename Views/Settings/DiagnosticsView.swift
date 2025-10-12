94027089aeb71a03b5aba3c3151124495413ca2d
Balazs Perlaki-Horvath <ba.perlaki@gmail.com>
2025-10-12 20:11:28 +0200

    Unify diagnostics view

--- Views/Settings/DiagnosticsView.swift
+++ Views/Settings/DiagnosticsView.swift
@@ -14,22 +14,42 @@
 // along with Kiwix; If not, see https://www.gnu.org/licenses/.
 
 import SwiftUI
-#if os(iOS)
-import MessageUI
-#endif
 
 /// NOTE: This view is not translated on purpose.
 /// We want to make sure users only send us reports in English
 struct DiagnosticsView: View {
     
     @State private var isLoading: Bool = false
-    
     private let alignment = HorizontalAlignment.leading
-#if os(iOS)
-    @State private var email: Email?
-#elseif os(macOS)
-    @State private var currentView: NSView?
+    
+    var body: some View {
+        VStack(alignment: alignment, spacing: 16) {
+            Spacer()
+            description
+            
+            HStack {
+                emailButton
+#if os(macOS)
+                saveButton
+#else
+                shareButton
 #endif
+                
+                if isLoading {
+                    Text("Please wait...")
+                }
+            }
+            Spacer()
+        }
+        .frame(maxWidth: 400)
+        .navigationTitle("Diagnostic Report")
+#if os(iOS)
+        .navigationBarTitleDisplayMode(.large)
+        .padding(.horizontal, 40)
+#else
+        .tabItem { Label("Diagnostics", systemImage: "exclamationmark.bubble") }
+#endif
+    }
     
     @ViewBuilder
     var description: some View {
@@ -64,63 +84,33 @@ struct DiagnosticsView: View {
 #endif
     }
     
+#if os(macOS)
+    @ViewBuilder
+    var saveButton: some View {
+        AsyncButton {
+            isLoading = true
+            defer { isLoading = false }
+            let logs = await Diagnostics.entries(separator: "\n")
+            guard let data = logs.data(using: .utf8) else { return }
+            let panel = NSSavePanel()
+            panel.allowedContentTypes = [.log]
+            panel.nameFieldStringValue = "diagnostics.log"
+            if case .OK = panel.runModal(),
+               let targetURL = panel.url {
+                try? data.write(to: targetURL)
+            }
+        } label: {
+            Label("Save log file", systemImage: "square.and.arrow.down")
+        }
+    }
+#endif
+    
+#if os(iOS)
     @ViewBuilder
     var shareButton: some View {
-#if os(macOS)
-        if let currentView {
-            AsyncButton {
-                isLoading = true
-                defer { isLoading = false }
-                let logs = await Diagnostics.entries(separator: "\n")
-                guard let data = logs.data(using: .utf8) else {
-                    return
-                }
-                let exportData = FileExportData(data: data, fileName: "diagnostics", fileExtension: "log")
-                guard let url = FileExporter.tempFileFrom(exportData: exportData) else {
-                    return
-                }
-                NSSharingServicePicker(items: [url]).show(
-                    relativeTo: NSRect(origin: CGPoint(x: 64, y: 330),
-                                       size: CGSize(width: 320, height: 54)
-                    ),
-                    of: currentView,
-                    preferredEdge: .minY
-                )
-            } label: {
-                Label("Share", systemImage: "square.and.arrow.up")
-            }
-        }
-#else
         // TODO: implement for iOS
         EmptyView()
-#endif
     }
+#endif
     
-    var body: some View {
-        VStack(alignment: alignment, spacing: 16) {
-            Spacer()
-            description
-            
-            HStack {
-                emailButton
-                shareButton
-                
-                if isLoading {
-                    Text("Please wait...")
-                }
-            }
-            Spacer()
-        }
-        .frame(maxWidth: 400)
-        .navigationTitle("Diagnostic Report")
-#if os(iOS)
-        .navigationBarTitleDisplayMode(.large)
-        .padding(.horizontal, 40)
-#else
-        .tabItem { Label("Diagnostics", systemImage: "exclamationmark.bubble") }
-        .withHostingWindow { hostWindow in
-            _currentView.wrappedValue = hostWindow?.contentView
-        }
-#endif
-    }
 }
