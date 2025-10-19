dab305c7fa4f8a24f49585ff0607582dcd84b94c
Balazs Perlaki-Horvath <ba.perlaki@gmail.com>
2025-10-20 00:54:22 +0200

    Spell correction within NSOperation

--- ViewModel/SearchViewModel.swift
+++ ViewModel/SearchViewModel.swift
@@ -160,7 +160,7 @@ final class SearchViewModel: NSObject, ObservableObject, NSFetchedResultsControl
         operation.completionBlock = { [weak self] in
             guard !operation.isCancelled else { return }
             Task { @MainActor [weak self] in
-                self?.results = await operation.searchResultItems
+                self?.results = operation.searchResultItems
                 self?.inProgress = false
             }
         }
