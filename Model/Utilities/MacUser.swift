d384ee141744c4422b863869caf5204844b849bc
Balazs Perlaki-Horvath <ba.perlaki@gmail.com>
2025-12-20 19:59:57 +0100

    Fix multi user crash scenario

--- Model/Utilities/MacUser.swift
+++ Model/Utilities/MacUser.swift
@@ -23,11 +23,17 @@ enum MacUser {
         Log.Environment.notice("MacUser name: \(userName, privacy: .public)")
     }
     
+    private static var isAdminCached: Bool?
     static func isUserAdmin() {
-        let backgroundQueue = DispatchQueue(label: "org.kiwix.macuser_background", qos: .background)
-        backgroundQueue.async {
-            let isAdmin = isAdmin()
-            Log.Environment.notice("MacUser isAdmin: \(isAdmin, privacy: .public)")
+        if let value = isAdminCached {
+            Log.Environment.notice("MacUser isAdmin: \(value, privacy: .public)")
+        } else {
+            let backgroundQueue = DispatchQueue(label: "org.kiwix.macuser_background", qos: .background)
+            backgroundQueue.async {
+                let value = isAdmin()
+                isAdminCached = value
+                Log.Environment.notice("MacUser isAdmin: \(value, privacy: .public)")
+            }
         }
     }
     
