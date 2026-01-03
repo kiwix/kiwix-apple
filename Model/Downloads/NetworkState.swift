4650356983d49b3d4851eeacfaa926064dbedcc0
Balazs Perlaki-Horvath <ba.perlaki@gmail.com>
2026-01-03 18:51:57 +0100

    Reflect offline state in dowloads
    Update log
    
    Update log

---
+++ Model/Downloads/NetworkState.swift
@@ -0,0 +1,47 @@
+// This file is part of Kiwix for iOS & macOS.
+//
+// Kiwix is free software; you can redistribute it and/or modify it
+// under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 3 of the License, or
+// any later version.
+//
+// Kiwix is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+// General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Kiwix; If not, see https://www.gnu.org/licenses/.
+
+import Foundation
+import Network
+
+final class NetworkState: ObservableObject {
+    @MainActor
+    @Published
+    var isOnline: Bool = true
+    
+    private let monitor = NWPathMonitor()
+    private let queue = DispatchQueue(label: "org.kiwix.network.monitor")
+    
+    init() {
+        monitor.pathUpdateHandler = { [weak self] path in
+            let newValue = path.status == .satisfied
+            Task { @MainActor [weak self] in
+                if newValue != self?.isOnline {
+                    self?.isOnline = newValue
+                }
+            }
+        }
+    }
+    
+    func startMonitoring() {
+        guard monitor.queue == nil else { return }
+        monitor.start(queue: queue)
+    }
+    
+    func stopMonitoring() {
+        guard monitor.queue != nil else { return }
+        monitor.cancel()
+    }
+}
