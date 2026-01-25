1a6324299641ec26aae49272e4bceb3e8b0f98af
Balazs Perlaki-Horvath <ba.perlaki@gmail.com>
2026-01-25 15:12:23 +0100

    Fix ZimFileService concurrency warnings

--- Support/Kiwix-Bridging-Header.h
+++ Support/Kiwix-Bridging-Header.h
@@ -2,7 +2,7 @@
 //  Use this file to import your target's public headers that you would like to expose to Swift.
 //
 
-#import "ZimFileService.h"
+#import "ZimService.h"
 #import "OPDSParser.h"
 #import "ZimFileMetaData.h"
 #import "SearchOperation.h"
