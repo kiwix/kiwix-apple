5fc23301e6df068216545965f566121948e4d9ba
Aryan Bhati <codiearyan07@gmail.com>
2026-02-11 14:26:24 +0530

    fix: added a default "article" title for print

--- Views/Buttons/PrintButton.swift
+++ Views/Buttons/PrintButton.swift
@@ -18,33 +18,33 @@ import SwiftUI
 import PDFKit
 
 struct PrintButton: View {
-    @FocusedValue(\.isBrowserURLSet) var isBrowserURLSet
-    let articleTitle: () -> String?
-    /// browser.webView.pdf()
-    let browserDataAsPDF: () async throws -> Data?
+	@FocusedValue(\.isBrowserURLSet) var isBrowserURLSet
+	let articleTitle: () -> String?
+	/// browser.webView.pdf()
+	let browserDataAsPDF: () async throws -> Data?
 
-    private func tempFileURL() async -> URL? {
-        guard let pdfData = try? await browserDataAsPDF(),
-              let title = articleTitle(), !title.isEmpty else { return nil }
-        return FileExporter.tempFileFrom(exportData: .init(data: pdfData, fileName: title.slugifiedFileName))
-    }
+	private func tempFileURL() async -> URL? {
+		guard let pdfData = try? await browserDataAsPDF(),
+			  let title = articleTitle(), !title.isEmpty else { return "article"}
+		return FileExporter.tempFileFrom(exportData: .init(data: pdfData, fileName: title.slugifiedFileName))
+	}
 
-    var body: some View {
-        Button {
-            Task {
-                guard let url = await tempFileURL() else { return }
-                let pdfDoc = PDFDocument(url: url)
-                let operation = pdfDoc?.printOperation(for: .shared, scalingMode: .pageScaleToFit, autoRotate: true)
-                operation?.run()
-            }
-        } label: {
-            Label {
-                Text(LocalString.common_button_print)
-            }  icon: {
-                Image(systemName: "printer")
-            }
-        }.disabled(isBrowserURLSet != true)
-        .keyboardShortcut("p", modifiers: .command)
-    }
+	var body: some View {
+		Button {
+			Task {
+				guard let url = await tempFileURL() else { return }
+				let pdfDoc = PDFDocument(url: url)
+				let operation = pdfDoc?.printOperation(for: .shared, scalingMode: .pageScaleToFit, autoRotate: true)
+				operation?.run()
+			}
+		} label: {
+			Label {
+				Text(LocalString.common_button_print)
+			}  icon: {
+				Image(systemName: "printer")
+			}
+		}.disabled(isBrowserURLSet != true)
+			.keyboardShortcut("p", modifiers: .command)
+	}
 }
 #endif
