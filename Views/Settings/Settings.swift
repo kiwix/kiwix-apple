ae3d189bcf38412f297eab01785c2c5d8ea54ecb
Balazs Perlaki-Horvath <ba.perlaki@gmail.com>
2026-02-15 14:17:28 +0100

    Group Settings misc and backup

--- Views/Settings/Settings.swift
+++ Views/Settings/Settings.swift
@@ -179,9 +179,8 @@ struct Settings: View {
                         readingSettings
                         downloadSettings
                         catalogSettings
-                        miscellaneous
                         hotspot.id("hotspot")
-                        backupSettings
+                        miscellaneous
                     } else {
                         readingSettings
                         miscellaneous
@@ -278,20 +277,8 @@ struct Settings: View {
         }
     }
 
-    var backupSettings: some View {
-        Section {
-            Toggle(LocalString.backup_settings_toggle_title, isOn: $backupDocumentDirectory)
-        } header: {
-            Text(LocalString.backup_settings_header_text)
-        } footer: {
-            Text(LocalString.backup_settings_footer_text)
-        }.onChange(of: backupDocumentDirectory) { _, newValue in
-            LibraryOperations.applyFileBackupSetting(isEnabled: newValue)
-        }
-    }
-
     var miscellaneous: some View {
-        Section(LocalString.settings_miscellaneous_title) {
+        Section {
             if Payment.paymentButtonType() != nil, horizontalSizeClass != .regular {
                 SupportKiwixButton {
                     openDonation()
@@ -309,6 +296,14 @@ struct Settings: View {
                 NavigationLink("Diagnostic report") { DiagnosticsView() }
             }
             NavigationLink(LocalString.settings_miscellaneous_navigation_about) { About() }
+            Toggle(LocalString.backup_settings_toggle_title, isOn: $backupDocumentDirectory)
+                .onChange(of: backupDocumentDirectory) { _, newValue in
+                    LibraryOperations.applyFileBackupSetting(isEnabled: newValue)
+                }
+        } header: {
+            Text(LocalString.settings_miscellaneous_title)
+        } footer: {
+            Text(LocalString.backup_settings_footer_text)
         }
     }
     
