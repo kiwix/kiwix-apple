dd51dacd35ecf7a1f6ae3015195286d3c9c92962
Balazs Perlaki-Horvath <ba.perlaki@gmail.com>
2026-03-08 09:00:24 +0100

    Break down iPad splitview to files

---
+++ App/SplitViewForiPad.swift
@@ -0,0 +1,273 @@
+// This file is part of Kiwix for iOS & macOS.
+//
+// Kiwix is free software; you can redistribute it and/or modify it
+// under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 3 of the License, or
+// any later version.
+//
+// Kiwix is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+// General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Kiwix; If not, see https://www.gnu.org/licenses/.
+
+#if os(iOS)
+import Foundation
+import SwiftUI
+
+@MainActor
+struct SplitViewForiPad: View {
+    @EnvironmentObject var navigation: NavigationViewModel
+    @State private var allSections: [MenuSection] = MenuSection.allMenuSections
+    @State private var menuDict: [MenuSection: [MenuItem]] = MenuSection.staticDictionary
+    @State private var selection: MenuItem?
+    @State private var navPath = NavigationPath()
+    @FetchRequest(
+        sortDescriptors: [NSSortDescriptor(key: "created", ascending: true)],
+        predicate: Tab.Predicate.notMissing(),
+        animation: .easeInOut
+    ) private var tabs: FetchedResults<Tab>
+    private let selectFileById = NotificationCenter.default.publisher(for: .selectFile)
+
+    var body: some View {
+        NavigationSplitView {
+            List(selection: $selection) {
+                ForEach(allSections) { (section: MenuSection) in
+                    sectionFor(section)
+                }
+            }
+            .listStyle(.sidebar)
+            .navigationTitle(Brand.appName)
+            .toolbar {
+                Menu {
+                    Button(role: .destructive) {
+                        guard case let .tab(tabID) = navigation.currentItem else { return }
+                        Task { [weak navigation] in
+                            await navigation?.deleteTab(tabID: tabID)
+                        }
+                    } label: {
+                        Label(LocalString.common_tab_menu_close_this, systemImage: "xmark.square")
+                    }
+                    Button(role: .destructive) {
+                        Task { [weak navigation] in
+                            await navigation?.deleteAllTabs()
+                        }
+                    } label: {
+                        Label(LocalString.common_tab_menu_close_all, systemImage: "xmark.square.fill")
+                    }
+                } label: {
+                    Label(LocalString.common_tab_menu_new_tab, systemImage: "plus.square")
+                } primaryAction: {
+                    navigation.createTab()
+                }
+            }
+        } detail: {
+            NavigationStack(path: $navPath) {
+                switch selection {
+                case .tab(let tabID):
+                    BrowserTab(tabID: tabID).id(tabID)
+                case .bookmarks:
+                    Bookmarks()
+                case .opened:
+                    ZimFilesOpened()
+                case .categories:
+                    ZimFilesCategories(dismiss: nil)
+                case .new:
+                    ZimFilesNew(dismiss: nil)
+                case .downloads:
+                    ZimFilesDownloads(dismiss: nil)
+                case let .settings(scrollToHotspot):
+                    Settings(scrollToHotspot: scrollToHotspot)
+                case .donation:
+                    // this won't be triggered
+                    EmptyView()
+                case .hotspot:
+                    HotspotZimFilesSelection()
+                case nil:
+                    EmptyView()
+                }
+            }
+        }
+        .task {
+            await loadDonations()
+            await observeOpeningFiles()
+        }
+        .onChange(of: navigation.currentItem) { _, newValue in
+            updateSelection(newValue)
+        }
+        // open file details, after importing file
+        .onReceive(selectFileById, perform: { notification in
+            guard let fileId = notification.userInfo?["fileId"] as? UUID else {
+                return
+            }
+            navPath.append(fileId)
+        })
+    }
+    
+    @ViewBuilder
+    private func sectionFor(_ section: MenuSection) -> some View {
+        if section == .tabs {
+            Section {
+                ForEach(tabs) { (tab: Tab) in
+                    NavigationLink(value: MenuItem.tab(objectID: tab.objectID)) {
+                        labelFor(tab: tab)
+                    }.id(tab.id)
+                }
+                .onDelete(perform: deleteTab)
+            }
+        } else {
+            if let sectionItems: [MenuItem] = menuDict[section] {
+                Section {
+                    ForEach(sectionItems, id: \.id) { (item: MenuItem) in
+                        navigationLinkFor(item: item)
+                            .id(item.id)
+                    }
+                } header: {
+                    if let headerText = section.header {
+                        Text(headerText)
+                    }
+                }
+            }
+        }
+    }
+    
+    @ViewBuilder
+    private func navigationLinkFor(item: MenuItem) -> some View {
+        if case .donation = item {
+            Button {
+                // trigger the donation pop-up, but do not select the menu item itself
+                NotificationCenter.openDonations()
+            } label: {
+                labelFor(item: .donation)
+            }
+        } else {
+            NavigationLink(value: item) {
+                labelFor(item: item)
+            }
+            .id(item.id)
+        }
+    }
+    
+    @ViewBuilder
+    private func labelFor(tab: Tab) -> some View {
+        let text = tab.title ?? LocalString.common_tab_menu_new_tab
+        let image: UIImage = {
+            if let zimFile = tab.zimFile, let category = Category(rawValue: zimFile.category) {
+                if let imgData = zimFile.faviconData {
+                    UIImage(data: imgData) ?? UIImage(systemName: "square")!
+                } else {
+                    UIImage(named: category.icon) ?? UIImage(systemName: "square")!
+                }
+            } else {
+                UIImage(systemName: "square")!
+            }
+        }()
+        
+        Label {
+            Text(text)
+                .lineLimit(1)
+                .truncationMode(.tail)
+        } icon: {
+            Image(uiImage: image)
+                .resizable()
+                .frame(maxWidth: 22, maxHeight: 22)
+                .clipShape(RoundedRectangle(cornerSize: CGSize(width: 3, height: 3)))
+        }
+    }
+    
+    @ViewBuilder
+    private func labelFor(item: MenuItem) -> some View {
+        let isSelected: Bool = item == selection
+        switch item {
+        case .tab:
+            // we use labelFor(tab:) instead
+            EmptyView()
+        case .donation:
+            Label {
+                Text(item.name)
+            } icon: {
+                Image(systemName: item.icon)
+                    .foregroundStyle(Color.red)
+                    .symbolVariant(isSelected ? .fill : .none)
+                    .modifier(Symbol26Vairant())
+            }
+        default:
+            Label(item.name, systemImage: item.icon)
+                .symbolVariant(isSelected ? .fill : .none)
+                .modifier(Symbol26Vairant())
+        }
+    }
+    
+    private static func allItems() -> [MenuItem] {
+        if FeatureFlags.hasLibrary {
+            return [.bookmarks, .opened, .categories, .downloads, .new, .hotspot, .settings(scrollToHotspot: false)]
+        } else {
+            return [.bookmarks, .hotspot, .settings(scrollToHotspot: false)]
+        }
+    }
+    
+    private func loadDonations() async {
+        guard allSections.contains(.donation) else { return }
+        if await Payment.paymentButtonTypeAsync() != nil {
+            menuDict[.donation] = [.donation]
+        }
+    }
+    
+    private func deleteTab(at offsets: IndexSet) {
+        for offset in offsets {
+            if 0 <= offset && offset < tabs.count {
+                let tab = tabs[offset]
+                Task {
+                    await navigation.deleteTab(tabID: tab.objectID)
+                }
+            }
+        }
+    }
+    
+    private func updateSelection(_ newNavItem: NavigationItem?) {
+        if let newNavItem, let newSelection = MenuItem(from: newNavItem) {
+            if selection != newSelection {
+                selection = newSelection
+            }
+        }
+    }
+    
+    private func observeOpeningFiles() async {
+        // open main page or open in new tab via long tap
+        for await notification in NotificationCenter.default.notifications(named: .openURL) {
+            guard let url = notification.userInfo?["url"] as? URL else { return }
+            let inNewTab = notification.userInfo?["inNewTab"] as? Bool ?? false
+            let deepLinkId: UUID?
+            if case .deepLink(.some(let linkID)) = notification.userInfo?["context"] as? OpenURLContext {
+                deepLinkId = linkID
+            } else {
+                deepLinkId = nil
+            }
+            Task { @MainActor in
+                if !inNewTab, case let .tab(tabID) = navigation.currentItem {
+                    BrowserViewModel.getCached(tabID: tabID).load(url: url)
+                } else {
+                    let tabID = navigation.createTab()
+                    BrowserViewModel.getCached(tabID: tabID).load(url: url)
+                }
+                if let deepLinkId {
+                    DeepLinkService.shared.stopFor(uuid: deepLinkId)
+                }
+            }
+        }
+    }
+}
+
+private struct Symbol26Vairant: ViewModifier {
+    func body(content: Content) -> some View {
+        if #available(iOS 26, *) {
+            content.symbolColorRenderingMode(.gradient)
+        } else {
+            content
+        }
+    }
+}
+
+#endif
