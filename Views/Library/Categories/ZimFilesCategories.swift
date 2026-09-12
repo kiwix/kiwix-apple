22a3130f0fb85b52392a0b58d486d3ae15112488
Balazs Perlaki-Horvath <ba.perlaki@gmail.com>
2026-09-12 14:09:28 +0200

    Unify ZIM categories to grid listing

--- Views/Library/Categories/ZimFilesCategories.swift
+++ Views/Library/Categories/ZimFilesCategories.swift
@@ -45,7 +45,7 @@ struct ZimFilesCategories: View {
     }
 
     var body: some View {
-        ZimFilesCategory(category: $selected, languageCode: $languageCode, dismiss: dismiss)
+        ZimFilesCategory(category: $selected, selectedLanguage: $languageCode, dismiss: dismiss)
             .modifier(ToolbarRoleBrowser())
             .navigationTitle(MenuItem.categories.name)
             .toolbar {
@@ -69,30 +69,74 @@ struct ZimFilesCategories: View {
     }
 }
 
-/// A grid or list of zim files under a single category.
+/// A grid of zim files under a single category, or as a search result
 struct ZimFilesCategory: View {
-    @Binding var category: Category
-    @Binding var languageCode: String
     @State private var searchText = ""
+    @Binding var category: Category
+    @Binding var selectedLanguage: String
+    @Default(.libraryLanguageCodes) private var languageCodes
+    @EnvironmentObject private var selection: SelectedZimFileViewModel
+    @Environment(\.horizontalSizeClass) private var horizontalSizeClass
+    @FetchRequest private var results: FetchedResults<ZimFile>
     let dismiss: (() -> Void)? // iOS only
+    
+    init(
+        category: Binding<Category>,
+        selectedLanguage: Binding<String>,
+        dismiss: (() -> Void)?
+    ) {
+        self._category = category
+        self._selectedLanguage = selectedLanguage
+        self._results = FetchRequest<ZimFile>(
+            sortDescriptors: [SortDescriptor(\ZimFile.name), SortDescriptor(\.size, order: .reverse)],
+            predicate: ZimFilesCategory.buildPredicate(
+                category: category.wrappedValue,
+                searchText: "",
+                languageCode: selectedLanguage.wrappedValue
+            ),
+            animation: .easeInOut
+        )
+        self.dismiss = dismiss
+    }
 
     var body: some View {
-        if category == .ted || category == .stackExchange || category == .other {
-            CategoryList(
-                category: $category,
-                selectedLanguage: $languageCode,
-                searchText: $searchText,
-                dismiss: dismiss
-            )
-        } else {
-            CategoryGrid(
-                category: $category,
-                selectedLanguage: $languageCode,
-                searchText: $searchText,
-                dismiss: dismiss
-            )
+        Group {
+            if results.isEmpty {
+                CategoryEmptySection()
+            } else {
+                LazyVGrid(columns: ([Self.gridItem]), alignment: .leading, spacing: 12) {
+                    ForEach(results, id: \.fileID) { zimFile in
+                        LibraryZimFileContext(
+                            content: { ZimFileCell(
+                                zimFile,
+                                prominent: .name,
+                                isSelected: selection.isSelected(zimFile)
+                            )},
+                            zimFile: zimFile,
+                            selection: selection,
+                            dismiss: dismiss)
+                    }
+                }.modifier(GridCommon())
+            }
+        }
+        .searchable(text: $searchText, prompt: LocalString.common_search)
+        .onChange(of: category) { selection.reset() }
+        .onChange(of: searchText) { _, newValue in
+            results.nsPredicate = ZimFilesCategory
+                .buildPredicate(category: category, searchText: newValue, languageCode: selectedLanguage)
+        }
+        .onChange(of: selectedLanguage) {
+            results.nsPredicate = ZimFilesCategory
+                .buildPredicate(category: category, searchText: searchText, languageCode: selectedLanguage)
+        }
+        .onChange(of: languageCodes) {
+            if !languageCodes.contains(selectedLanguage) {
+                selectedLanguage = languageCodes.first ?? "eng"
+            }
         }
     }
+    
+    private static let gridItem = GridItem(.adaptive(minimum: 250, maximum: 500), spacing: 12)
 
     @MainActor
     static func buildPredicate(
@@ -102,205 +146,15 @@ struct ZimFilesCategory: View {
     ) -> NSPredicate {
         let regex = String(format: "(.*,)?%@(,.*)?", languageCode)
         let langPredicate = NSPredicate(format: "languageCode MATCHES %@", regex)
-        var predicates = [
-            NSPredicate(format: "category == %@", category.rawValue),
-            langPredicate,
-            NSPredicate(format: "requiresServiceWorkers == false")
-        ]
+        var predicates: [NSPredicate] = []
+        if searchText.isEmpty {
+            predicates.append(NSPredicate(format: "category == %@", category.rawValue))
+        }
+        predicates.append(langPredicate)
+        predicates.append(NSPredicate(format: "requiresServiceWorkers == false"))
         if !searchText.isEmpty {
             predicates.append(NSPredicate(format: "name CONTAINS[cd] %@", searchText))
         }
         return NSCompoundPredicate(andPredicateWithSubpredicates: predicates)
-    }
-}
-
-private struct CategoryGrid: View {
-    @Binding var category: Category
-    @Binding var selectedLanguage: String
-    @Binding var searchText: String
-    @Default(.libraryLanguageCodes) private var languageCodes
-    @EnvironmentObject private var selection: SelectedZimFileViewModel
-    @Environment(\.horizontalSizeClass) private var horizontalSizeClass
-    @SectionedFetchRequest private var sections: SectionedFetchResults<String, ZimFile>
-    private let dismiss: (() -> Void)? // iOS only
-
-    init(
-        category: Binding<Category>,
-        selectedLanguage: Binding<String>,
-        searchText: Binding<String>,
-        dismiss: (() -> Void)?
-    ) {
-        self._selectedLanguage = selectedLanguage
-        self._category = category
-        self._searchText = searchText
-        self.dismiss = dismiss
-        self._sections = SectionedFetchRequest<String, ZimFile>(
-            sectionIdentifier: \.name,
-            sortDescriptors: [SortDescriptor(\ZimFile.name), SortDescriptor(\.size, order: .reverse)],
-            predicate: ZimFilesCategory.buildPredicate(
-                category: category.wrappedValue,
-                searchText: searchText.wrappedValue,
-                languageCode: selectedLanguage.wrappedValue
-            ),
-            animation: .easeInOut
-        )
-    }
-
-    var body: some View {
-        Group {
-            if sections.isEmpty {
-                CategoryEmptySection()
-            } else {
-                LazyVGrid(columns: ([gridItem]), alignment: .leading, spacing: 12) {
-                    ForEach(sections) { section in
-                        Section {
-                            ForEach(section, id: \.fileID) { zimFile in
-                                LibraryZimFileContext(
-                                    content: { ZimFileCell(
-                                        zimFile,
-                                        prominent: .size,
-                                        isSelected: selection.isSelected(zimFile)
-                                    )
-                                    },
-                                    zimFile: zimFile,
-                                    selection: selection,
-                                    dismiss: dismiss)
-                            }
-                        } header: {
-                            SectionHeader(
-                                title: section.id,
-                                category: Category(rawValue: section.first?.category) ?? .other,
-                                imageData: section.first?.faviconData,
-                                imageURL: section.first?.faviconURL
-                            ).padding(
-                                EdgeInsets(
-                                    top: section.id == sections.first?.id ? 0 : 10,
-                                    leading: 12,
-                                    bottom: -6,
-                                    trailing: 0
-                                )
-                            )
-                        }
-                    }
-                }.modifier(GridCommon())
-            }
-        }
-        .searchable(text: $searchText, prompt: LocalString.common_search)
-        .onChange(of: category) { selection.reset() }
-        .onChange(of: searchText) { _, newValue in
-            sections.nsPredicate = ZimFilesCategory
-                .buildPredicate(category: category, searchText: newValue, languageCode: selectedLanguage)
-        }
-        .onChange(of: selectedLanguage) {
-            sections.nsPredicate = ZimFilesCategory
-                .buildPredicate(category: category, searchText: searchText, languageCode: selectedLanguage)
-        }
-        .onChange(of: languageCodes) {
-            if !languageCodes.contains(selectedLanguage) {
-                selectedLanguage = languageCodes.first ?? "eng"
-            }
-        }
-    }
-
-    private var gridItem: GridItem {
-        if horizontalSizeClass == .regular {
-            return GridItem(.adaptive(minimum: 200, maximum: 400), spacing: 12)
-        } else {
-            return GridItem(.adaptive(minimum: 175, maximum: 400), spacing: 12)
-        }
-    }
-
-    private struct SectionHeader: View {
-        let title: String
-        let category: Category
-        let imageData: Data?
-        let imageURL: URL?
-
-        var body: some View {
-            Label {
-                Text(title).font(.title3).fontWeight(.semibold)
-            } icon: {
-                Favicon(category: category, imageData: imageData, imageURL: imageURL).frame(height: 20)
-            }
-        }
-    }
-}
-
-private struct CategoryList: View {
-    @Binding var category: Category
-    @Binding var selectedLanguage: String
-    @Binding var searchText: String
-    @Default(.libraryLanguageCodes) private var languageCodes
-    @EnvironmentObject private var selection: SelectedZimFileViewModel
-    @FetchRequest private var zimFiles: FetchedResults<ZimFile>
-    private let dismiss: (() -> Void)?
-
-    init(category: Binding<Category>,
-         selectedLanguage: Binding<String>,
-         searchText: Binding<String>,
-         dismiss: (() -> Void)?
-    ) {
-        self._selectedLanguage = selectedLanguage
-        self._category = category
-        self._searchText = searchText
-        self.dismiss = dismiss
-        self._zimFiles = FetchRequest<ZimFile>(
-            sortDescriptors: [
-                NSSortDescriptor(
-                    key: "name", ascending: true, selector: #selector(NSString.localizedCaseInsensitiveCompare)
-                ),
-                NSSortDescriptor(keyPath: \ZimFile.size, ascending: false)
-            ],
-            predicate: ZimFilesCategory.buildPredicate(
-                category: category.wrappedValue,
-                searchText: searchText.wrappedValue,
-                languageCode: selectedLanguage.wrappedValue
-            ),
-            animation: .easeInOut
-        )
-    }
-
-    var body: some View {
-        Group {
-            if zimFiles.isEmpty {
-                CategoryEmptySection()
-            } else {
-                List(zimFiles, id: \.self, selection: $selection.selectedZimFile) { zimFile in
-                    LibraryZimFileContext(
-                        content: { ZimFileRow(zimFile) },
-                        zimFile: zimFile,
-                        selection: selection,
-                        dismiss: dismiss)
-                }
-                #if os(macOS)
-                .listStyle(.inset)
-                #elseif os(iOS)
-                .listStyle(.plain)
-                #endif
-            }
-        }
-        .searchable(text: $searchText, prompt: LocalString.common_search)
-        .onChange(of: category) { selection.reset() }
-        .onChange(of: searchText) { _, newValue in
-            zimFiles.nsPredicate = ZimFilesCategory
-                .buildPredicate(
-                    category: category,
-                    searchText: newValue,
-                    languageCode: selectedLanguage
-                )
-        }
-        .onChange(of: selectedLanguage) {
-            zimFiles.nsPredicate = ZimFilesCategory
-                .buildPredicate(
-                    category: category,
-                    searchText: searchText,
-                    languageCode: selectedLanguage
-                )
-        }
-        .onChange(of: languageCodes) {
-            if !languageCodes.contains(selectedLanguage) {
-                selectedLanguage = languageCodes.first ?? "eng"
-            }
-        }
     }
 }
