cd0385d881d57c4a9e4aba49502b9bea908ff0be
Balazs Perlaki-Horvath <ba.perlaki@gmail.com>
2026-09-12 14:09:28 +0200

    Unify ZIM categories to grid listing

--- Views/Library/Categories/ZimFilesCategories.swift
+++ Views/Library/Categories/ZimFilesCategories.swift
@@ -102,7 +102,7 @@ struct ZimFilesCategory: View {
             sortDescriptors: [SortDescriptor(\ZimFile.name), SortDescriptor(\.size, order: .reverse)],
             predicate: ZimFilesCategory.buildPredicate(
                 category: category.wrappedValue,
-                searchText: searchText.wrappedValue,
+                searchText: "",
                 languageCode: selectedLanguage.wrappedValue
             ),
             animation: .easeInOut
